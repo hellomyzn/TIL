1473f05a26dd73c40216ad2ba6ee459e3b6b8d68
宮園英志 <miyazonoeiji@miyazonoeiji-no-MacBook-Air.local>
2017-06-04 16:34:16 +0900

    Created Shot

---
+++ Assets/Scripts/AK47.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK47 : MonoBehaviour {
+	AudioClip getItemSound;
+	AudioSource audioSource;
+	public RayControl rayControl;
+
+
+	// Use this for initialization
+	void Start () {
+		getItemSound = Resources.Load<AudioClip>("Audio/fire");
+		audioSource  = GetComponent<AudioSource>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		if(Input.GetMouseButtonDown(0)){
+			if (rayControl.bulletInterval >= 0.3f) {
+				audioSource.PlayOneShot(getItemSound);
+			}
+		}
+		if(Input.GetMouseButtonUp(0)){
+
+		}
+	}
+}
---
+++ Assets/Scripts/RayControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayControl : MonoBehaviour {
+	public GameObject sparkle;
+	public Renderer sparkle2;
+	public float bulletInterval;
+	GameObject bullet;
+
+
+	// Use this for initialization
+	void Start () {
+		sparkle2 = sparkle2.GetComponent<Renderer>();
+		bulletInterval = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bulletInterval += Time.deltaTime;
+		if(Input.GetMouseButtonDown(0)){
+			if (bulletInterval >= 0.3f) {
+				GenerateBullet ();
+			}				
+		}	
+
+		if(Input.GetMouseButtonUp(0)){
+			sparkle2.enabled = false;
+			Destroy(bullet);
+		}					
+		print(bulletInterval);	
+	}
+
+	void GenerateBullet(){
+		bulletInterval = 0.0f;
+		sparkle2.enabled = true;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)){			
+			bullet = Instantiate(sparkle,hit.point, Quaternion.identity);
+		}
+	}
+}
