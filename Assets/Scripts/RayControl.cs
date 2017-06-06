c9ce126302d64f474436e67b0ff7dae6230a23e5
hellomyzn <em711th@icloud.com>
2017-06-07 06:47:43 +0900

    修正１（Ray）

--- Assets/Scripts/RayControl.cs
+++ Assets/Scripts/RayControl.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class RayControl : MonoBehaviour {
 	public GameObject sparkle;
 	public Renderer sparkle2;
+	public GameObject firstPersonCharacter;
 	GameObject bullet;
-
+	Camera camera;
+	
+	
 
 	// Use this for initialization
 	void Start () {
 		sparkle2 = sparkle2.GetComponent<Renderer>();
+		camera = firstPersonCharacter.GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +23,7 @@ public class RayControl : MonoBehaviour {
 
 	public void GenerateBullet(){
 		sparkle2.enabled = true;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Ray ray = camera.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
 		Debug.DrawRay(ray.origin, ray.direction, Color.red, 3.0f);
 		RaycastHit hit;
 		if(Physics.Raycast(ray, out hit)){			
