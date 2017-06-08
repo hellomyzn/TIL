28cb4cb3c522bccd5e862531dd31a38f1cf318d8
hellomyzn <em711th@icloud.com>
2017-06-08 21:01:49 +0900

    修正！！！！！

--- Assets/Scripts/RayControl.cs
+++ Assets/Scripts/RayControl.cs
@@ -6,8 +6,10 @@ public class RayControl : MonoBehaviour {
 	public GameObject sparkle;
 	public Renderer sparkle2;
 	public GameObject firstPersonCharacter;
-	public int targetLife;
-	public int score;
+	public GameObject scoreManager;
+	public GameObject target;
+	Target targetScript;
+	ScoreManager scoreManagerScript;
 	GameObject bullet;
 	Camera camera;
 	
@@ -17,31 +19,39 @@ public class RayControl : MonoBehaviour {
 	void Start () {
 		sparkle2 = sparkle2.GetComponent<Renderer>();
 		camera = firstPersonCharacter.GetComponent<Camera>();
-		score = 0;
-		targetLife = 5;
+		scoreManagerScript = scoreManager.GetComponent<ScoreManager>();
+		targetScript = target.GetComponent<Target>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
-
 	public void GenerateBullet(){
 		sparkle2.enabled = true;
 		Ray ray = camera.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
-		Debug.DrawRay(ray.origin, ray.direction, Color.red, 3.0f);
+		Debug.DrawRay (ray.origin, ray.direction * 100, Color.red, 3, false);
 		RaycastHit hit;
 		if(Physics.Raycast(ray, out hit)){			
-			bullet = Instantiate(sparkle,hit.point, Quaternion.identity);
-			print(hit.collider.gameObject.name + hit.point);
+			Vector3 bulletPosition = (hit.point - ray.origin).normalized;
+			bullet = Instantiate(sparkle,hit.point - bulletPosition, Quaternion.identity);
+			Vector3 hitPoint = hit.point;
+			Vector3 targetPoint = hit.collider.gameObject.transform.position;
+			// print(hit.collider.gameObject.name + hitPoint + targetPoint);
+
 			if(hit.collider.gameObject.name == "pCube1"){
-				targetLife--;
-				score += 20;
-			}else if(hit.collider.gameObject.name == "HeadMarkerTarget"){
-				targetLife--;
-				score += 100;
+				targetScript.targetLife --;
+				scoreManagerScript.AddScore();
+				
+			}else if(hit.collider.gameObject.name == "HeadMarkerSensor"){
+				targetScript.targetLife--;
+				float HeadMarkerPoint= (targetPoint - hitPoint).magnitude * 10;
+				if(HeadMarkerPoint < 1){
+					scoreManagerScript.AddHeadMarkerHigh();
+				}else{
+					scoreManagerScript.AddHeadMarkerLow();
+				}
 			}
 		}
-		
 	}
   
   public void DestroyBullet(){
