4a45e65888545749ba68a07b5df7166548c23cad
MyznEiji <em711th@icloud.com>
2017-11-20 11:48:46 +0900

    sampleFlower.py新規作成

---
+++ tc-ai/sampleFlower.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
+import pandas as pd
+iris = load_iris()
+
+pd.DataFrame(data = iris.data,columns = iris.feature_names)
+x = iris.data
+x_train,x_test,y_train,y_test = train_test_split(x,test_size = 0.3)
