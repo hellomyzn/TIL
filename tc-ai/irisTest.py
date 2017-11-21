861bd7c8ec601fbecf7812d91433bb5f445fd064
MyznEiji <em711th@icloud.com>
2017-11-21 13:37:47 +0900

    std.pyを新規追加

--- tc-ai/irisTest.py
+++ tc-ai/irisTest.py
@@ -8,15 +8,13 @@ import pandas as pd
 import numpy as np
 
 
-
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=0
-)
+    X, y, test_size=0.3, random_state=0)
 lr = LogisticRegression(C=1000.0, random_state=0)
 lr.fit(X_train, y_train)
 y_p = lr.predict(X_test)
--- tc-ai/std.py
+++ tc-ai/std.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
-#%matplotlib inline
+
 
 iris = datasets.load_iris()
 x = iris.data[:, [2, 3]]
@@ -57,6 +57,53 @@ plt.scatter(x_test_std[:, 0],
 
 plt.xlabel('Petal Length')
 plt.ylabel('Petal Width')
+
+plt.legend(loc='lower right')
+
+plt.show()
+
+x_concat_std = np.concatenate((x_train_std, x_test_std), axis=0)
+x_min, x_max = x_concat_std[:, 0].min() - 1, x_concat_std[:, 0].max() + 1
+y_min, y_max = x_concat_std[:, 1].min() - 1, x_concat_std[:, 1].max() + 1
+print(x_min, x_max)
+print(y_min, y_max)
+x_axis, y_axis = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                             np.arange(y_min, y_max, 0.02))
+
+print(x_axis.shape)
+print(y_axis.shape)
+
+data_num = x_axis.shape[0] * x_axis.shape[1]
+print(data_num)
+
+grid_points = np.concatenate(
+    (x_axis.reshape(data_num, 1), y_axis.reshape(data_num, 1)), axis=1)
+
+class_labels = lr.predict(grid_points)
+class_labels = class_labels.reshape(x_axis.shape)
+
+plt.xlim(x_axis.min(), x_axis.max())
+plt.ylim(y_axis.min(), x_axis.max())
+
+plt.contourf(x_axis, y_axis, class_labels, alpha=0.3, cmap=cmap)
+
+for i, n in enumerate(np.unique(y)):
+    plt.scatter(x=x_train_std[y_train == n, 0],
+                y=x_train_std[y_train == n, 1],
+                c=cmap(i),
+                marker=markers[i],
+                s=70,
+                edgecolors='',
+                label=n)
+
+plt.scatter(x=x_test_std[:, 0],
+            y=x_test_std[:, 1],
+            c='k',
+            marker='v',
+            label='test data')
+
+plt.xlabel('Petal Length')
+plt.ylabel('Patel Width')
 
 plt.legend(loc='lower right')
 
