3207012783c2fa670880d3ef20de90b772491637
MyznEiji <em711th@icloud.com>
2017-12-06 17:15:59 +0900

    Fine titanicのデータ分析

---
+++ hogehoge.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+im = Image.open("サーバーの歴史.png")
+# box=(左上のx座標, 左上のy座標, 右下のx座標, 右下のy座標)
+im_crop = im.crop((10, 20, 100, 200))
+im_crop.show()
