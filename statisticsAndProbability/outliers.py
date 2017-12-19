f6a68a37736b00737e791b8bd8dcadfe93085fb2
MyznEiji <em711th@icloud.com>
2017-12-19 19:54:20 +0900

    fixed outliers.py

--- statisticsAndProbability/outliers.py
+++ statisticsAndProbability/outliers.py
@@ -19,3 +19,33 @@ plt.hist(incomes, 50)
 ヒストグラム上においてその他の人々を一本の線に押し込めてしまいました。その上、
 収入の平均までおかしくしています。
 """
+
+incomes.mean()
+
+"""
+外れ値がなぜ発生し、どこから来るのか理解するのは大事なことです。そして、
+これを除去してもいいのか解析の目的と照らし合わせて考える必要があります。もし、
+典型的な市民を解析し理解したいのであれば、大富豪を除去していもいいように思えます。
+
+実は、大富豪を除去するよりも良い方法があります。
+データセットの中央値よりも標準偏差2つ分より離れた値を除去する方法です。
+"""
+
+
+def reject_outliers(data):
+    u = np.median(data)
+    s = np.std(data)
+    filtered = [e for e in data if (u - 2 * s < e < u + 2 * s)]
+    return filtered
+
+
+filtered = reject_outliers(incomes)
+
+plt.hist(filtered, 50,edgecolor="k")
+
+
+"""
+良くなりましたね。平均値も、意味をなすようになりました
+"""
+
+np.mean(filtered)
