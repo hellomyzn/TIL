afdf5c16c2094d081083bbdf5489487aeddc4d7f
MyznEiji <em711th@icloud.com>
2018-01-26 16:34:30 +0900

    Fine Delegate

---
+++ tc-design-and-design/Assets/Scripts/TechCamp/Delegate/Dog.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Dog : MonoBehaviour 
+{
+
+	private Vector3 _target;
+
+	public delegate void DogDelegate(string text);
+
+	public DogDelegate OnAlived;
+
+	public void Go(Vector3 target)
+	{
+		_target = target;
+	}
+
+	private void Update()
+	{
+		transform.position += (_target - transform.position).normalized * Time.deltaTime;
+		
+		if((transform.position - _target).magnitude < 0.1f)
+		{
+			OnAlived("なでなで");
+		}
+	}
+	//void Start () 
+	//{
+	//	
+	//}
+	
+	//void Update () 
+	//{
+	//	
+	//}
+}
---
+++ tc-design-and-design/Assets/Scripts/TechCamp/Delegate/Owner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owner : MonoBehaviour 
+{
+	[SerializeField] private Dog _dog;
+	private void Start()
+	{
+		_dog.Go(transform.position);
+
+		_dog.OnAlived += (text) =>
+		{
+			Debug.Log(text);
+		};
+	}
+	//void Start () 
+	//{
+	//	
+	//}
+	
+	//void Update () 
+	//{
+	//	
+	//}
+}
