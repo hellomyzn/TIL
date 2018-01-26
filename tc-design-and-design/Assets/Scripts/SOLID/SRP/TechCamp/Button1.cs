17c267b150b47eb2ed9ec3fa7f24598852a116dd
MyznEiji <em711th@icloud.com>
2018-01-26 17:27:07 +0900

    Fine SRP

---
+++ tc-design-and-design/Assets/Scripts/SOLID/SRP/TechCamp/Button1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Button1 : MonoBehaviour, IButton
+{
+	public void Press()
+	{
+		print("ボタン1が押されました。");
+	}
+	//void Start () 
+	//{
+	//	
+	//}
+	
+	//void Update () 
+	//{
+	//	
+	//}
+}
