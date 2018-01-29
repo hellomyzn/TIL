d5b820da90270573aef7736d5df949bbccdb98dd
MyznEiji <em711th@icloud.com>
2018-01-30 00:26:18 +0900

    fine ISP

---
+++ tc-design-and-design/Assets/Scripts/SOLID/ISP/TechCamp/ISettableCharacterStatus.cs
@@ -0,0 +1,11 @@
+public interface ISettableCharacterStatus
+{
+    int HP { get; set; }
+    int MP { get; set; }
+    int ATK { get; set; }
+    int DEF { get; set; }
+    int INT { get; set; }
+    int RES { get; set; }
+    int SPD { get; set; }
+
+}
