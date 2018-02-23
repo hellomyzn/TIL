9072da138a23898efa5c2180d41e3d3323750b70
MyznEiji <em711th@icloud.com>
2018-02-23 15:52:14 +0900

    add : marker

--- u-petsHuggyRails/app/controllers/pages_controller.rb
+++ u-petsHuggyRails/app/controllers/pages_controller.rb
@@ -1,5 +1,25 @@
 class PagesController < ApplicationController
-  def index
+  def index #views/index.html.erbを表示させるというアクション
     @users = User.all
+  end
+
+  def search
+    if params[:search].present?
+
+      @latitude = params["lat"]
+      @longitude = params["lng"]
+
+      geolocation = [@latitude,@longitude]
+
+      @listings = Listing.where(active: true).near(geolocation, 1, order: 'distance')
+
+    # 検索欄が空欄の場合
+    else
+
+      @listings = Listing.where(active: true).all
+      @latitude = @listings.to_a[0].latitude
+      @longitude = @listings.to_a[0].longitude
+
+    end
   end
 end
