4855c6b4eceea476548339ba7c4a0fc996516b9c
MyznEiji <em711th@icloud.com>
2018-03-19 18:48:28 +0900

    first commit

---
+++ myzn_mooovi/app/Http/Controllers/RankingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class RankingController extends Controller
+{
+    public $layout = "layouts.review_site";
+}
