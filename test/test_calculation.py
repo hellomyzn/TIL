cdb882377f2cd2491bcdae23c4cfed17b6b4c644
hellomyzn <em711th@icloud.com>
2018-04-13 18:09:42 +0900

    add setup tearDown

--- test/test_calculation.py
+++ test/test_calculation.py
@@ -4,9 +4,23 @@ import calculationPython
 
 
 class CalTest(unittest.TestCase):
+
+    def setUp(self):
+        print('setup')
+        self.cal = calculationPython.Call()
+
+    def tearDown(self):
+        print('clean up')
+        del self.cal
+
     def test_add_num_and_double(self):
-        cal = calculationPython.Call()
-        self.assertEqual(cal.add_num_add_double(1, 1), 4)
+        self.assertEqual(self.cal.add_num_add_double(1, 1), 4)
+
+    def test_add_num_and_double_raise(self):
+        with self.assertRaises(ValueError):
+            self.cal.add_num_add_double('1', '1')
+
+
 
 
 if __name__ == '__main__':
