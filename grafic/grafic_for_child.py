13da455654f614c714c5fc17d5bf807f70efa089
hellomyzn <em711th@icloud.com>
2018-05-06 20:18:07 +0900

    add : tkinterPython

--- grafic/grafic_for_child.py
+++ grafic/grafic_for_child.py
@@ -15,16 +15,24 @@ turtle.right(90)
 # 亀のアイコンを隠します
 turtle.hideturtle()
 turtle.forward(100)
+# 亀のアイコンを表示する
 turtle.showturtle()
+
+# 円を書いて進みます
 turtle.circle(100)
 
 time.sleep(2)
 
+# 軌跡の色を変更します
 turtle.pencolor('blue')
 turtle.right(45)
 turtle.forward(100)
+# 後ろに進みます
 turtle.backward(200)
 
+#　初期位置に戻ります
 turtle.home()
+# タートルのクリア
 turtle.clear()
+# タートルの終了コマンド　
 turtle.done()
