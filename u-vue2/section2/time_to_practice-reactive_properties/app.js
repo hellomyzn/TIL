218534585c279b94196720bbd6b38f614cc81e66
MyznEiji <em711th@icloud.com>
2018-07-18 16:47:53 +0900

    fine time_to_practice_reactive_properties

--- u-vue2/section2/time_to_practice-reactive_properties/app.js
+++ u-vue2/section2/time_to_practice-reactive_properties/app.js
@@ -1,6 +1,23 @@
+window.onload = function(){
+
 new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        watch: {
+          value: function(value){
+            var vm = this;
+            setTimeout(function(){
+              vm.value = 0;
+              }, 5000);
+          }
+        },
+        methods: {
+          results: function(){
+            return this.value == 37 ? 'done' : 'not there yet';
+          }
         }
     });
+}
+
