7aa36ae0ee72e70cf747e9f8b7b2cc7a4885a365
MyznEiji <em711th@icloud.com>
2018-07-18 17:52:11 +0900

    fine dynamic_styling_with

--- u-vue2/section2/dynamic_styling_with_css/js/style.js
+++ u-vue2/section2/dynamic_styling_with_css/js/style.js
@@ -3,6 +3,14 @@ window.onload = function(){
     el:'#app',
     data: {
       attachRed: false
+    },
+    computed: {
+      divClasses: function(){
+        return {
+          red: this.attachRed,
+          blue: !this.attachRed
+        };
+      }
     }
 
   });
