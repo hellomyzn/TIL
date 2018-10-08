602d0bfd66fa7b8141d40da4f6e10bf33afbcf48
宮園英志 <miyazonoeiji@miyazonoeiji-no-MacBook-Air.local>
2018-10-08 10:34:15 +0800

    separate js

---
+++ vue_getting_started/section4_router/user_list_spa/user_list.js
@@ -0,0 +1,50 @@
+
+      var getUsers = function (callback){
+        setTimeout(function() {
+          callback(null, [
+            {
+              id: 1,
+              name: 'Takuya Tejima'
+            },
+            {
+              id: 2,
+              name: 'Yohei Noda'
+            }
+          ])
+         }, 1000)
+       }
+
+      var UserList = {
+        template: '#user-list',
+        data: function() {
+          return {
+            loading: false,
+            users: function() {return []},
+            error: null
+          }
+        },
+
+        created: function(){
+          this.fetchData()
+        },
+
+        watch: {
+          '$route': 'fetchData'
+        },
+
+        methods: {
+          fetchData: function(){
+            this.loading = true
+
+            getUsers((function(err, users){
+              this.loading = false
+              if(err){
+                this.error = err.toString()
+              } else {
+                this.users = users
+              }
+            }).bind(this))
+          }
+        }
+      }
+
