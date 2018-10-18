e78d87d6782f9ddf58441387328593685d2619d7
Eiji_Myzn <em711th@icloud.com>
2018-10-18 22:01:06 +0900

    find find_by

---
+++ b-rails4/app/controllers/record_controller.rb
@@ -0,0 +1,11 @@
+class RecordController < ApplicationController
+  def find
+    @books = Book.find([2, 5, 10])
+    render 'hello/list'
+  end
+
+  def find_by
+    @book = Book.find_by(publish: '技術評論社')
+    render 'books/show'
+  end
+end
