41e3b52a3341b4f2054eea69da3e410acce4a2fa
MyznEiji <em711th@icloud.com>
2019-01-26 11:58:12 +0900

    seek

--- 2nd_try_dec_2018/chapter08_file_system/with.py
+++ 2nd_try_dec_2018/chapter08_file_system/with.py
@@ -1,2 +1,22 @@
+s = """\
+AAA
+BBB
+CCC
+DDD
+        """
+
 with open("test.txt", "w") as f:
-    f.write('eiji\n')
+    f.write(s)
+
+with open("test.txt", 'r') as f:
+    print(f.tell())
+    print(f.read(1))
+    f.seek(5)
+    print(f.tell())
+    print(f.read(1))
+    f.seek(14)
+    print(f.read(1))
+    f.seek(15)
+    print(f.read(1))
+    f.seek(5)
+    print(f.read(1))
