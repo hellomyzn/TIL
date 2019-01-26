d8d65c5272065f53701a9a9250cf65b1ff659020
hellomyzn <em711th@icloud.com>
2019-01-26 12:04:43 +0900

    w+ r+

--- 2nd_try_dec_2018/chapter08_file_system/with.py
+++ 2nd_try_dec_2018/chapter08_file_system/with.py
@@ -4,19 +4,7 @@ BBB
 CCC
 DDD
         """
-
-with open("test.txt", "w") as f:
+with open('test.txt', "w+") as f:
     f.write(s)
-
-with open("test.txt", 'r') as f:
-    print(f.tell())
-    print(f.read(1))
-    f.seek(5)
-    print(f.tell())
-    print(f.read(1))
-    f.seek(14)
-    print(f.read(1))
-    f.seek(15)
-    print(f.read(1))
-    f.seek(5)
-    print(f.read(1))
+    f.seek(0)
+    print(f.read())
