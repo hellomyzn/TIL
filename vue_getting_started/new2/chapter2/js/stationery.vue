4b5e7aef456f9880a613b6f2494d68ac45e64592
MyznEiji <em711th@icloud.com>
2019-06-12 09:25:49 +0000

    filters

--- vue_getting_started/new2/chapter2/js/stationery.vue
+++ vue_getting_started/new2/chapter2/js/stationery.vue
@@ -37,6 +37,16 @@ var vm = new Vue({
     },
     totalPriceWithTax: function(){
       return Math.floor(this.totalPrice * 1.08)
+    },
+    canBuy: function(){
+      return this.totalPrice >= 1000
+    },
+    errorMessageStyle: function() {
+      // canBuyが偽の時に赤く表示する
+      return {
+        border: this.canBuy ? '' : '1px solid red',
+        color: this.canBuy ? '' : 'red'
+      }
     }
   }
 })
