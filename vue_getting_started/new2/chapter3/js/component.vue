73d3b8174340bef3531926ce256446f9de7525cd
MyznEiji <em711th@icloud.com>
2019-06-16 07:59:12 +0000

    component

---
+++ vue_getting_started/new2/chapter3/js/component.vue
@@ -0,0 +1,39 @@
+var headerTemplate = `
+  <div style="color: red;">
+    <slot name="header">*親から何も帰ってこない場合、この文章が表示される</slot>
+  </div>
+`
+var headerTemplate2 = `
+  <div>
+    <slot name="header2">No Title</slot>
+  </div>
+`
+
+
+var contentTemplate = `
+  <div>
+    <slot name="content">No Title</slot>
+  </div>
+`
+
+
+
+Vue.component('page-header', {
+  template: headerTemplate
+})
+
+Vue.component('page-header2', {
+  template: headerTemplate2
+})
+
+Vue.component('page-content', {
+  template: contentTemplate
+})
+
+new Vue({
+  el: '#fruits-list2'
+})
+
+new Vue({
+  el: "#fruits-list"
+})
