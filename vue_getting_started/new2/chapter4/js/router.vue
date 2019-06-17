16c47041e79feb70b63860a9f5365f7c4edb5296
MyznEiji <em711th@icloud.com>
2019-06-17 01:01:33 +0000

    router

---
+++ vue_getting_started/new2/chapter4/js/router.vue
@@ -0,0 +1,36 @@
+var router = new VueRouter({
+  routes: [
+    {
+      path: '/top',
+      component: {
+        template: '<div>トップページです。</div>'
+      }
+    },
+    {
+      path: '/users',
+      component: {
+        template: '<div>ユーザー一覧ページです。</div>'
+      }
+    },
+    {
+      // コロンで始まるパターンマッチング
+      path: '/user/:userId',
+      component: {
+        template: '<div>ユーザーIDは {{ $route.params.userId }}です。</div>'
+      }
+    },
+    	// 名前付きルート
+    {
+      path: '/user/:userId',
+      name: 'user',
+      component: {
+        template: '<div>ユーザーIDは {{ $route.params.userId }}です。 </div>'
+      }
+    }
+  ]
+})
+
+// ルーターのインスタンスをrootとなるVueインスタンスに渡す
+var app = new Vue({
+  router: router
+}).$mount('#app')
