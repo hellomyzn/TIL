e657181cbbfe11ef2ec4f288e9c4de1674db542a
MyznEiji <em711th@icloud.com>
2019-06-20 13:44:01 +0000

    add : hello world

---
+++ go_in_super_speed/chapter3/hello_world.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func init(){
+    fmt.Println("INIT!")
+}
+
+func buzz(){
+    fmt.Println('Buzz')
+}
+
+func main(){
+    buzz()
+    fmt.Println("Hello world!", "TEST TEST")
+    // コメントアウト
+
+    /*
+    hogehogehoge
+    */
+}
+
