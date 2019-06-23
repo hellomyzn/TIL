1847275ce1c097c58160a5b46e57b0fd7e537361
MyznEiji <em711th@icloud.com>
2019-06-23 07:05:21 +0000

    variable

--- go_in_super_speed/chapter3/variable_declaration.go
+++ go_in_super_speed/chapter3/variable_declaration.go
@@ -3,10 +3,42 @@ package main
 import "fmt"
 
 func main() {
-    var i int = 1
-    var f64 float64 = 1.2
-    var s string = "test"
-    var t, f bool = true, false
-    fmt.Println(i, f64, s, t, f)
+    // var i int = 1
+    // var f64 float64 = 1.2
+    // var s string = "test"
+    // var t, f bool = true, false
+    // fmt.Println(i, f64, s, t, f)
+    //
+
+    // var (
+    //
+    // i int = 1
+    // f64 float64 = 1.2
+    // s string = "test"
+    // t, f bool = true, false
+    //
+    // )
+
+    // var (
+    //
+    // i int
+    // f64 float64
+    // s string
+    // t, f bool
+    //
+    // )
+
+
+
+    xi := 1
+    xf64 := 1.2
+    var xf32 float32 = 1.2
+    xs := "test"
+    xt, xf := true, false
+
+
+
+    fmt.Println(xi, xf64, xs, xt, xf)
+    fmt.Printf("%T", xf32)
 
 }
