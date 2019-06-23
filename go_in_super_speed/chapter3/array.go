7fe0bebb39fc11209bbdb69195226eb8da98d1e4
MyznEiji <em711th@icloud.com>
2019-06-23 10:35:04 +0000

    array

---
+++ go_in_super_speed/chapter3/array.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+    var a [2]int
+    a[0] = 100
+    a[1] = 200
+    fmt.Println(a)
+
+    var b [2]int = [2]int{100, 200}
+    // サイズ変更するとエラーが出る
+    // b = append(b,300)
+    fmt.Println(b)
+
+
+
+    var c []int = []int{100, 200}
+    c = append(c, 300)
+    fmt.Println(c)
+}
