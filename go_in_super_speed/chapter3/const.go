d5b3b1bc5e85331b722ed6b1cfe8d34ced6b7ec3
MyznEiji <em711th@icloud.com>
2019-06-23 07:27:22 +0000

    : const

---
+++ go_in_super_speed/chapter3/const.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+
+const Pi = 3.14
+
+const (
+    Username = "test user"
+    Password = "test pass"
+)
+
+
+func main() {
+    fmt.Println(Pi, Username, Password)
+}
