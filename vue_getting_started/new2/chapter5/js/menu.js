f83acc43012b75a3b62c1042790a49f332844540
MyznEiji <em711th@icloud.com>
2019-06-25 00:02:15 +0000

    vue

--- vue_getting_started/new2/chapter5/js/menu.js
+++ vue_getting_started/new2/chapter5/js/menu.js
@@ -19,8 +19,49 @@ var PullDownMenu ={
         @leave="leave"
         :css="false"
       >
-
+        <ul v-if="isShown">
+          <li v-for="item in items" :key="item">
+            <a href="#" class="menu-item">{{ item }}</a>
+          </li>
+        </ul>
       </transition>
     </div>
-  `
+  `,
+  methods: {
+    beforeEnter: function(el){
+      // el: トランジションの対象となるDOM要素
+      // アニメーションの初期状態(高さ透明度)の設定
+      el.style.height = '0px'
+      el.style.opacity = '0'
+    },
+    enter: function(el, done){
+      // el: トランジションの対象となるDOM要素
+      // 要素の高さを取得し、Anime.jsを用いてメニューを下に展開する
+      // 3秒かけて、透明度と高さを変更して出現させる
+      anime({
+        targets: el,
+        opacity: 1,
+        height: el.scrollHeight + 'px',
+        duration: 3000,
+        complate: done
+      })
+    },
+    leave: function(el, done){
+      // el: トランジションの対象となるDOM要素
+      anime({
+        targets: el,
+        opacity: 0,
+        height: '0px',
+        duration: 300,
+        complate: done
+      })
+    }
+  }
 }
+
+new Vue({
+  el: '#app',
+  components: {
+    PullDownMenu: PullDownMenu
+  }
+})
