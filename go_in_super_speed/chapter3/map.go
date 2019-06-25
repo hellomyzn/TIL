7e360df73193ab08a317e4a3281a1b0552182230
MyznEiji <em711th@icloud.com>
2019-06-25 05:51:38 +0000

    map

---
+++ go_in_super_speed/chapter3/map.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.PrintLn(m)
+	fmt.PrintLn(m["apple"])
+
+	m["banana"] = 300
+	fmt.PrintLn(m)
+	m["new"] = 500
+	fmt.PrintLn(m)
+
+	fmt.PrintLn(m["nothing"])
+
+	v, ok := m["apple"]
+	fmt.PrintLn(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.PrintLn(v2, ok2w)
+
+	m2 := make(map[string]int)
+	m2["pc"] = 5000
+	fmt.PrintLn(m2)
+
+	var m3 map[string]int
+	m3["pc"] = 5000
+	fmt.PrintLn(m3)
+
+	var s []int
+	if s == nil{
+		fmt.PrintLn("Nil")
+	}
+}
