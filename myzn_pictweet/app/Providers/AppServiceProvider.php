64f9405602e28af47adac1fc957dc77eb3931577
MyznEiji <em711th@icloud.com>
2019-08-12 04:46:40 +0900

    fix

--- myzn_pictweet/app/Providers/AppServiceProvider.php
+++ myzn_pictweet/app/Providers/AppServiceProvider.php
@@ -16,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+
+        Schema::defaultStringLength(191);
+
     }
 
     /**
