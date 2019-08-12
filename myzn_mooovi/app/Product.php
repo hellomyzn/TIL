d38c895dd29f2c246edfc7108a38feed55b2b611
MyznEiji <em711th@icloud.com>
2019-08-12 15:56:09 +0900

    add : reviea average

--- myzn_mooovi/app/Product.php
+++ myzn_mooovi/app/Product.php
@@ -9,4 +9,14 @@ class Product extends Model
     //
     protected $fillable = ['title', 'image_url', 'director', 'detail', 'open_date'];
 
+    public function reviews()
+    {
+        return $this->hasMany('App\Review');
+    }
+
+    public function review_average()
+    {
+        return round($this->reviews()->avg('rate'));
+    }
+
 }
--- myzn_mooovi/app/Review.php
+++ myzn_mooovi/app/Review.php
@@ -8,4 +8,15 @@ class Review extends Model
 {
     //
     protected $fillable = ['nickname', 'rate', 'review', 'product_id'];
+
+    public function product()
+    {
+
+        return $this->belongsTo('App\Product');
+    }
+
+    public function review_average()
+    {
+        return round($this->reviews()->avg('rate'));
+    }
 }
