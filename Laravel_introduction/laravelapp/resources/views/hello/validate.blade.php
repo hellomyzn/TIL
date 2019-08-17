932e79df020837a066b3b825bffbe1050d2b6912
MyznEiji <em711th@icloud.com>
2019-08-17 21:12:51 +0900

    error

--- Laravel_introduction/laravelapp/resources/views/hello/validate.blade.php
+++ Laravel_introduction/laravelapp/resources/views/hello/validate.blade.php
@@ -2,13 +2,7 @@
 
 @if (count($errors) > 0)
 
-<div>
-    <ul>
-    @foreach($errors->all() as $error)
-        <li>{{ $error }}</li>
-    @endforeach
-    </ul>
-</div>
+<p>入力に問題があります。</p>
 
 @endif
 
@@ -16,10 +10,36 @@
 <table>
 <form action="/hello/validate_post" method="post">
     {{ csrf_field() }}
+    @if ($errors->has('name'))
     <tr>
-        <th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
-        <th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
-        <th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
-        <tr><th></th><td><input type="submit" value="send"></td></tr>
+        <th>Error</th>
+        <td>{{$errors->first('name')}}</tdi
+    </tr>
+    @endif
+    <tr>
+        <th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td>
+    </tr>
+
+    @if ($errors->has('mail'))
+    <tr>
+        <th>Error</th>
+        <td>{{$errors->first('mail')}}</td>
+    </tr>
+    @endif
+    <tr>
+        <th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td>
+    </tr>
+
+    @if ($errors->has('age'))
+    <tr>
+        <th>Error</th>
+        <td>{{$errors->first('age')}}</td>
+    </tr>
+    @endif
+    <tr>
+        <th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td>
+    </tr>
+
+    <tr><th></th><td><input type="submit" value="send"></td></tr>
 </form>
 </table>
