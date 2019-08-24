46e44345f76b3bbc809dde58e9e0ccb20b8f3e38
MyznEiji <em711th@icloud.com>
2019-08-24 23:17:21 +0900

    picture

--- myzn_blog/resources/views/article/daily_index.blade.php
+++ myzn_blog/resources/views/article/daily_index.blade.php
@@ -1,52 +1,16 @@
 @extends('layout')
 
 @section('content')
-<div class="container w-75">
-
-
-    <div class="row justify-content-center  " >
-        <div class="col-md-8 bg-primaly pl-0">
-            <h1>Daily</h1>
-        </div>
-    </div>
-
-    <div class="row justify-content-center" style="height:450px;">
-        <div class="col-md-8 bg-info">
-            fuga
-        </div>
-    </div>
-
-    <div class="row justify-content-center mt-5" style="height: 350px;">
-        <div class="col-md-4 bg-success pl-0">
-            <div class="mr-3 bg-danger">
-                hoge
-            </div>
-
-        </div>
-
-        <div class="col-md-4 bg-danger pr-0">
-            <div class="ml-3 bg-success">
-                piyo
-            </div>
-        </div>
-    </div>
-
-    <div class="row justify-content-center mt-5" style="height: 350px;">
-        <div class="col-md-4 bg-danger">piyo</div>
-        <div class="col-md-4 bg-success">fuga</div>
-    </div>
-
-    <div class="row justify-content-center mt-5" style="height: 350px;">
-        <div class="col-md-4 bg-success">fuga</div>
-        <div class="col-md-4 bg-danger">piyo</div>
-    </div>
-
+    <div class="container">
     @foreach( $dailies as $daily )
-        <p>{{ $daily->title }}</p>
-        <p><img src="https://myzn-blog.s3-ap-southeast-1.amazonaws.com/{{ $daily->picture }}" alt=""></p>
-        <p>{{ $daily->date }}</p>
-        <p>{{ $daily->text }}</p>
+        <div class="row bg-info">
+                <p >{{ $daily->title }}</p>
+                <p >{{ $daily->picture}}</p>
+                <p>{{ $daily->date }}</p>
+                <p>{{ $daily->text }}</p>
+            </div>
+        </div>
     @endforeach
-</div>
+    </div>
 @endsection
 
