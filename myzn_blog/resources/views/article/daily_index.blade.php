93dae00ae09d3acc6c54796b8fccdd4383c1cafb
MyznEiji <em711th@icloud.com>
2019-08-24 21:06:01 +0900

    index :

--- myzn_blog/resources/views/article/daily_index.blade.php
+++ myzn_blog/resources/views/article/daily_index.blade.php
@@ -1,7 +1,45 @@
 @extends('layout')
 
 @section('content')
-<div class="container">INDEX DAILY
+<div class="container w-75">
+
+
+    <div class="row justify-content-center  " >
+        <div class="col-md-8 bg-primaly pl-0">
+            <h1>Daily</h1>
+        </div>
+    </div>
+
+    <div class="row justify-content-center" style="height:450px;">
+        <div class="col-md-8 bg-info">
+            fuga
+        </div>
+    </div>
+
+    <div class="row justify-content-center mt-5" style="height: 350px;">
+        <div class="col-md-4 bg-success pl-0">
+            <div class="mr-3 bg-danger">
+                hoge
+            </div>
+
+        </div>
+
+        <div class="col-md-4 bg-danger pr-0">
+            <div class="ml-3 bg-success">
+                piyo
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center mt-5" style="height: 350px;">
+        <div class="col-md-4 bg-danger">piyo</div>
+        <div class="col-md-4 bg-success">fuga</div>
+    </div>
+
+    <div class="row justify-content-center mt-5" style="height: 350px;">
+        <div class="col-md-4 bg-success">fuga</div>
+        <div class="col-md-4 bg-danger">piyo</div>
+    </div>
 
     @foreach( $dailies as $daily )
         <p>{{ $daily->title }}</p>
