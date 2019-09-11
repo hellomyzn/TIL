c00ba3b0b7a7fe5270378dabc97673489dda78cf
MyznEiji <em711th@icloud.com>
2019-09-11 21:53:28 +0900

    fix asset

--- myzn_blog/resources/views/deepbreath/index.blade.php
+++ myzn_blog/resources/views/deepbreath/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
-    <link rel="stylesheet" href="{{ asset('css/deepbreath/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('./css/deepbreath/style.css') }}">
 </head>
 <body>
 
@@ -12,7 +12,7 @@
         <p>audioに対応していない場合のメッセージ</p>
     </audio>
 
-    <div class="wrapper" style="background: url( {{ asset('image/see.jpeg') }} )  no-repeat center top fixed; background-size: cover;">
+    <div class="wrapper" style="background: url( {{ asset('./image/see.jpeg') }} )  no-repeat center top fixed; background-size: cover;">
 
         <div id="timer">00:00:000</div>
         <button id="start">start</button>
@@ -21,4 +21,4 @@
     </div>
 </body>
 </html>
-<script src="{{ asset('js/deepbreath.js') }}"></script>
+<script src="{{ asset('./js/deepbreath.js') }}"></script>
