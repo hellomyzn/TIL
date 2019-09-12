7177c2eecf45048e65166f1fbf100c34caec49a9
MyznEiji <em711th@icloud.com>
2019-09-12 16:38:03 +0900

    fix css

--- myzn_blog/resources/views/deepbreath/index.blade.php
+++ myzn_blog/resources/views/deepbreath/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
-    <link rel="stylesheet" href="{{ asset('./css/deepbreath/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/deepbreath/style.css') }}">
 </head>
 <body>
     <div class="wrapper">
