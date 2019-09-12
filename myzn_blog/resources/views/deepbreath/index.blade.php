03099bc3af6e58c0dd6188cf50d7a4db0db11923
MyznEiji <em711th@icloud.com>
2019-09-12 16:11:03 +0900

    add : mp4

--- myzn_blog/resources/views/deepbreath/index.blade.php
+++ myzn_blog/resources/views/deepbreath/index.blade.php
@@ -6,19 +6,21 @@
     <link rel="stylesheet" href="{{ asset('./css/deepbreath/style.css') }}">
 </head>
 <body>
+    <div class="wrapper">
+    <video src="{{ asset('mp4/see.mp4') }} " autoplay muted  loop></video>
+        <div class="timer">
+            <div id="timer">00:00:000</div>
+            <button id="start">start</button>
+            <button id="stop">stop</button>
+            <button id="reset">reset</button>
+        </div>
+    </div>
 
     <audio  autoplay loop >
         <source src="{{ asset('mp3/see.mp3') }}">
         <p>audioに対応していない場合のメッセージ</p>
     </audio>
 
-    <div class="wrapper" style="background: url( {{ asset('./image/see.jpeg') }} )  no-repeat center top fixed; background-size: cover;">
-
-        <div id="timer">00:00:000</div>
-        <button id="start">start</button>
-        <button id="stop">stop</button>
-        <button id="reset">reset</button>
-    </div>
 </body>
 </html>
 <script src="{{ asset('./js/deepbreath.js') }}"></script>
