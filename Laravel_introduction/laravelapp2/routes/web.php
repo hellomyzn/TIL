8eb100c85f0257978101b0cbf7ee4d952d103646
MyznEiji <em711th@icloud.com>
2020-02-12 02:06:52 +0000

    laravel

--- Laravel_introduction/laravelapp2/routes/web.php
+++ Laravel_introduction/laravelapp2/routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello/{msg?}', function ($msg="no message.") {
+    $html = <<<EOF
+    <html>
+    <head> 
+    <title>hello</title>
+    <style>
+        body {font-size:16px; color:#9999; }
+        h1 {font-size:100px; text-align:right; color: #eee;  margin:-40px 0px -50px 0px;}
+    </style>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <p>This is sample page</p>
+        <p>{$msg}</p>
+    </body>
+ 
+    </html>
+EOF;
+    return $html;
+});
