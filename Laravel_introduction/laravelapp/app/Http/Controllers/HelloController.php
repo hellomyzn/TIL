281f291bf067a5b0c740932fbc094f2cee518935
MyznEiji <em711th@icloud.com>
2020-02-29 04:18:30 +0000

    delete

--- Laravel_introduction/laravelapp/app/Http/Controllers/HelloController.php
+++ Laravel_introduction/laravelapp/app/Http/Controllers/HelloController.php
@@ -64,6 +64,20 @@ class HelloController extends Controller
         return redirect('hello');
     }
 
+    public function del(Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove(Request $request)
+    {
+        $param = ['id' => $request->id];
+        DB::delete('delete from people where id =:id', $param);
+        return redirect('/hello');
+    }
+    
     public function cookie(Request $request)
     {
         if ($request->hasCookie('msg'))
