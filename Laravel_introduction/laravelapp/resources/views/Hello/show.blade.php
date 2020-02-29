c97a8ca8b917b7afa8c51bc4d9f292d1d6f367ca
MyznEiji <em711th@icloud.com>
2020-02-29 10:59:17 +0000

    show

---
+++ Laravel_introduction/laravelapp/resources/views/Hello/show.blade.php
@@ -0,0 +1,9 @@
+<table>
+    <tr>
+        <th>id: </th><td>{{$items->id}}</td>
+        <th>name: </th><td>{{$items->name}}</td>
+        <th>mail: </th><td>{{$items->mail}}</td>
+        <th>age: </th><td>{{$items->age}}</td>
+    </tr>
+
+</table>
