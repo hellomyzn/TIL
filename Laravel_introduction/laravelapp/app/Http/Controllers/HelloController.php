5e9d912bf2f9ef1c4a712ce441b4e3641e238534
MyznEiji <em711th@icloud.com>
2020-03-01 06:42:19 +0000

    where raw

--- Laravel_introduction/laravelapp/app/Http/Controllers/HelloController.php
+++ Laravel_introduction/laravelapp/app/Http/Controllers/HelloController.php
@@ -74,12 +74,12 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-        $name = $request->name;
+        $min = $request->min;
+        $max = $request->max;
         $items = DB::table('people')
-            ->where('name', 'like', '%' . $name . '%')
-            ->orWhere('mail', 'like', '%' . $name . '%')
-            ->get();
+            ->whereRaw('age >= ? and age <= ?', [$min, $max])->get();
         return view('hello.show', ['items' => $items]);
+
     }
     
     public function cookie(Request $request)
