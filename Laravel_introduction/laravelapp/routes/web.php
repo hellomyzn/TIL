721f9c6de2f652924d171798e34569be9c8a86ea
MyznEiji <em711th@icloud.com>
2020-03-02 14:14:20 +0000

    find

--- Laravel_introduction/laravelapp/routes/web.php
+++ Laravel_introduction/laravelapp/routes/web.php
@@ -39,3 +39,6 @@ Route::get('hello/show', 'HelloController@show');
 
 
 Route::get('person', 'PersonController@index');
+
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');
