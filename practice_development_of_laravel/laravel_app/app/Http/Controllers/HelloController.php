5aaee92167faf94dc844ec691998e34915242457
MyznEiji <em711th@icloud.com>
2020-03-09 00:20:51 +0000

    sample

--- practice_development_of_laravel/laravel_app/app/Http/Controllers/HelloController.php
+++ practice_development_of_laravel/laravel_app/app/Http/Controllers/HelloController.php
@@ -8,8 +8,11 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
+        $sample_msg = config('sample.message');
+        $sample_data = config('sample.data');
         $data = [
-            "msg" => $request->hello
+            'msg' => $sample_msg,
+            'data' => $sample_data
         ];
 
         return view('hello.index', $data);
