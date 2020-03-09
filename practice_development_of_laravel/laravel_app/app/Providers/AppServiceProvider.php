33e41f022aeb0a535efb8a524986865e18c17176
MyznEiji <em711th@icloud.com>
2020-03-09 00:42:22 +0000

    service providor

--- practice_development_of_laravel/laravel_app/app/Providers/AppServiceProvider.php
+++ practice_development_of_laravel/laravel_app/app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        config([
+            'sample.data'=>['こんんいちは', 'どうも', 'さようなら']
+        ]);
     }
 }
