585c99ae15ff7e1deae01106d628c0d81fc9dc12
MyznEiji <em711th@icloud.com>
2020-05-17 13:50:26 +0000

    crud

--- myzn_blog/routes/web.php
+++ myzn_blog/routes/web.php
@@ -14,3 +14,5 @@
 Route::get('contact', 'PageController@getContact');
 Route::get('about', 'PageController@getAbout');
 Route::get('/', 'PageController@getIndex');
+
+Route::resource('posts', 'PostController');
