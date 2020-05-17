241c855fc93c91c2a95f7e39ba414bdbb8999501
MyznEiji <em711th@icloud.com>
2020-05-17 09:04:40 +0000

    contact me

--- myzn_blog/resources/views/pages/about.blade.php
+++ myzn_blog/resources/views/pages/about.blade.php
@@ -55,6 +55,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h1>About Me</h1>
+                <hr>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugit porro consectetur deserunt. Quae debitis corrupti nam eveniet odit ipsum laborum minus, expedita, sequi eius optio iure voluptatum necessitatibus ab.
                 </p>
