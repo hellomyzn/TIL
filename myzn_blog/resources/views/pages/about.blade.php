51ecea76adfba06cfb55381e0b8e0d2e991067d3
MyznEiji <em711th@icloud.com>
2020-05-17 10:35:03 +0000

    layouts

--- myzn_blog/resources/views/pages/about.blade.php
+++ myzn_blog/resources/views/pages/about.blade.php
@@ -1,72 +1,13 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('main')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-
-    <title>Laravel Blog - About Me</title>
-    </head>
-    <body>
-
-    {{-- Default Bootstrap Navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Laravel Blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
-
-            </ul>
-
-            <ul class="my-2 my-lg-0 navbar-nav">
-                <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        My Account
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div>
-            </ul>
-
-        </div>
-    </nav>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>About Me</h1>
-                <hr>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugit porro consectetur deserunt. Quae debitis corrupti nam eveniet odit ipsum laborum minus, expedita, sequi eius optio iure voluptatum necessitatibus ab.
-                </p>
-            </div>
-        </div>
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <h1>About Me</h1>
+        <hr>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates fugit porro consectetur deserunt. Quae debitis corrupti nam eveniet odit ipsum laborum minus, expedita, sequi eius optio iure voluptatum necessitatibus ab.
+        </p>
     </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
-</html>
+</div>
+@endsection
--- myzn_blog/resources/views/pages/welcome.blade.php
+++ myzn_blog/resources/views/pages/welcome.blade.php
@@ -1,118 +1,59 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('main')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-
-    <title>Laravel Blog</title>
-    </head>
-    <body>
-
-    {{-- Default Bootstrap Navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Laravel Blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact</a>
-                </li>
-
-            </ul>
-
-            <ul class="my-2 my-lg-0 navbar-nav">
-                <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        My Account
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div>
-            </ul>
-
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="jumbotron">
+            <h1 class="display-4">Wellcome to My Blog!</h1>
+            <p class="lead">Thank you so much for visting. This is my test website built with Laravel. Please road my popular post </p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a>
         </div>
-    </nav>
+    </div>
+</div>
 
+<div class="row">
+    <div class="col-md-8">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="jumbotron">
-                    <h1 class="display-4">Wellcome to My Blog!</h1>
-                    <p class="lead">Thank you so much for visting. This is my test website built with Laravel. Please road my popular post </p>
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a>
-                </div>
-            </div>
+        <div class="post">
+            <h3>Post Title</h3>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
+            </p>
+            <a href="" class="btn btn-primary">Read More</a>
         </div>
+        <hr>
 
-        <div class="row">
-            <div class="col-md-8">
+        <div class="post">
+            <h3>Post Title</h3>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
+            </p>
+            <a href="" class="btn btn-primary">Read More</a>
+        </div>
+        <hr>
 
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
-                    </p>
-                    <a href="" class="btn btn-primary">Read More</a>
-                </div>
-                <hr>
+        <div class="post">
+            <h3>Post Title</h3>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
+            </p>
+            <a href="" class="btn btn-primary">Read More</a>
+        </div>
+        <hr>
 
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
-                    </p>
-                    <a href="" class="btn btn-primary">Read More</a>
-                </div>
-                <hr>
+        <div class="post">
+            <h3>Post Title</h3>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
+            </p>
+            <a href="" class="btn btn-primary">Read More</a>
+        </div>
+        <hr>
+    </div> {{-- end of .col-md-8 --}}
 
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
-                    </p>
-                    <a href="" class="btn btn-primary">Read More</a>
-                </div>
-                <hr>
+    <div class="col-md-3 col-md-offset-1" >
+        <h2>Sidebar</h2>
+    </div>
+</div> {{-- end of .row --}}
 
-                <div class="post">
-                    <h3>Post Title</h3>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde veniam ipsa impedit autem dolorum quas, quos, nulla nihil distinctio mollitia cum voluptates suscipit praesentium aut dolorem quibusdam? Nam, eaque! Est.
-                    </p>
-                    <a href="" class="btn btn-primary">Read More</a>
-                </div>
-                <hr>
-            </div> {{-- end of .col-md-8 --}}
-
-            <div class="col-md-3 col-md-offset-1" >
-                <h2>Sidebar</h2>
-            </div>
-        </div> {{-- end of .row --}}
-    </div> {{-- end of .container  --}}
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
