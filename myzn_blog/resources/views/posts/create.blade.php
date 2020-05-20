febe5d9da05755b77ec8c423bcafce47cc58ed39
MyznEiji <em711th@icloud.com>
2020-05-20 13:48:37 +0000

    creat page

---
+++ myzn_blog/resources/views/posts/create.blade.php
@@ -0,0 +1,12 @@
+@extends('../layouts/main')
+
+@section('title', '| Create New Post')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Create New Post</h1>
+            <hr>
+        </div>
+    </div>
+@endsection
