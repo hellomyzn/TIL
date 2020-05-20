0c6ce75688ea24b5d3846524641202c5f882950e
MyznEiji <em711th@icloud.com>
2020-05-20 13:56:30 +0000

    finish form for post

--- myzn_blog/resources/views/posts/create.blade.php
+++ myzn_blog/resources/views/posts/create.blade.php
@@ -7,6 +7,16 @@
         <div class="col-md-8 col-md-offset-2">
             <h1>Create New Post</h1>
             <hr>
+
+            {{ Form::open(['route' => 'posts.store']) }}
+                {{ Form::label('title', 'Title:')}}
+                {{ Form::text('title', null, array('class'=> 'form-control'))}}
+
+                {{ Form::label('body', "Post Body:")}}
+                {{ Form::textarea('body', null, array('class'=>'form-control'))}}
+
+                {{ Form::submit('Create Post')}}
+            {{ Form::close() }}
         </div>
     </div>
 @endsection
