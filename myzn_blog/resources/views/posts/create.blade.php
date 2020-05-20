9d06600fc1c46bfc9694a067195d8e107787b18c
MyznEiji <em711th@icloud.com>
2020-05-20 14:07:13 +0000

    fix : form

--- myzn_blog/resources/views/posts/create.blade.php
+++ myzn_blog/resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 offset-md-2">
             <h1>Create New Post</h1>
             <hr>
 
@@ -15,7 +15,7 @@
                 {{ Form::label('body', "Post Body:")}}
                 {{ Form::textarea('body', null, array('class'=>'form-control'))}}
 
-                {{ Form::submit('Create Post')}}
+                {{ Form::submit('Create Post', array('class'=>'btn btn-success btn-lg btn-block', 'style'=>'margin-top: 20px;'))}}
             {{ Form::close() }}
         </div>
     </div>
