c56ed983f7ca64fa57bddf0b91cfec9e34aaa1f4
MyznEiji <em711th@icloud.com>
2020-07-15 00:46:08 +0000

    edit and delete on view

--- myzn_blog/backend/resources/views/posts/show.blade.php
+++ myzn_blog/backend/resources/views/posts/show.blade.php
@@ -2,6 +2,37 @@
 
 @section('title', '| View Post')
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <dl class="row">
+                        <dt class="col-5">Create at:</dt>
+                        <dd class="col-7">{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+                    </dl>
+                    <dl class="row">
+                        <dt class="col-5">Updated at:</dt>
+                        <dd class="col-7">{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+                    </dl>
+
+                    <hr>
+                    <div class="row">
+
+                        <div class="col-sm-6">
+                            {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+                        </div>
+                        <div class="col-sm-6">
+                            {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
