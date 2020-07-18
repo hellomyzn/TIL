c6202d40721a028c6847e85cacb353b7a042daa7
MyznEiji <em711th@icloud.com>
2020-07-18 01:14:29 +0000

    list of posts

--- myzn_blog/backend/resources/views/posts/index.blade.php
+++ myzn_blog/backend/resources/views/posts/index.blade.php
@@ -10,8 +10,38 @@
         </div>
 
         <div class="col-md-2">
-            <a href="{{ route('posts.create') }}" class="btn btn-block  btn-primary">Create New Post</a>
+            <a href="{{ route('posts.create') }}" class="btn btn-block btn-primary btn-h1-spacing">Create New Post</a>
         </div>
-        <hr>
+        <div class="col-md-12">
+            <hr>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Created at</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach($posts as $post)
+                        <tr>
+                        <th>{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ substr($post->body, 0, 50) }} {{ strlen($post->body) > 50 ? "..." : ""}}</td>
+                        <td>{{ date('M j, Y', strtotime($post->created_at)) }}</td>
+                        <td>
+                            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default">View</a>
+                            <a href="{{ route('posts.edit', $post->id)}}" class="btn btn-default">Edit</a>
+                        </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @stop
