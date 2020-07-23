449be1ab2c35310570efe6cda1e84e54c23607de
MyznEiji <em711th@icloud.com>
2020-07-23 22:39:23 +0000

    add paginate

--- myzn_blog/backend/resources/views/posts/index.blade.php
+++ myzn_blog/backend/resources/views/posts/index.blade.php
@@ -42,6 +42,10 @@
                     @endforeach
                 </tbody>
             </table>
+
+            <div class="d-flex justify-content-center">
+                {!! $posts->links(); !!}
+            </div>
         </div>
     </div>
 @stop
