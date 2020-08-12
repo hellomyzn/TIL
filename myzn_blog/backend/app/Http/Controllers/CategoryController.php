12fda4d9061149eeb2eaa7a8b25a4ea6d251f7fa
MyznEiji <em711th@icloud.com>
2020-08-12 13:32:23 +0000

    category add function

--- myzn_blog/backend/app/Http/Controllers/CategoryController.php
+++ myzn_blog/backend/app/Http/Controllers/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use Session;
 
 class CategoryController extends Controller
 {
@@ -21,7 +23,17 @@ class CategoryController extends Controller
     }
 
     public function store(Request $request){
+        $this->validate($request, array(
+            'name' => 'required|max:255'
+        ));
 
+        $category = new Category;
+
+        $category->name = $request->name;
+        $category->save();
+
+        Session::flash('success', 'New Category has been created');
+        return redirect()->route('categories.index');
     }
 
 }
