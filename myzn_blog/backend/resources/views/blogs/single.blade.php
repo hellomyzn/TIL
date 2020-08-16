042182ad38df41e6d335102daba692cea63eede9
MyznEiji <em711th@icloud.com>
2020-08-16 09:05:21 +0000

    front for comment

--- myzn_blog/backend/resources/views/blogs/single.blade.php
+++ myzn_blog/backend/resources/views/blogs/single.blade.php
@@ -17,10 +17,24 @@
 
     <div class="row">
         <div class="col-md-8 offset-md-2">
+            <h3 class="comment-title">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chat-left-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                </svg>
+                {{ $post->comments()->count() }} Comments
+            </h3>
             @foreach ($post->comments as $comment)
                 <div class="comment">
-                    <p><strong>Name:</strong> {{ $comment->name }}</p>
-                    <p><strong>Comment:</strong><br> {{ $comment->comment }}</p> <br>
+                    <div class="author-info">
+                        <img src="{{ "https://www.gravatar.com/avatar/" . md5(strtolower(trim($comment->email)) ) . "?s=50&d=mp"  }}" alt="" class="author-image">
+                        <div class="author-name">
+                            <h4>{{ $comment->name }}</h4>
+                            <p class="author-time">{{ date('F nS, Y - g:iA', strtotime($comment->created_at))  }}</p>
+                        </div>
+                    </div>
+                    <div class="comment-content">
+                        {{ $comment->comment }}
+                    </div>
                 </div>
             @endforeach
         </div>
