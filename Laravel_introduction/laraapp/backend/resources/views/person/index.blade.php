99fad9ab0d54ce09030200d7760174a53a1c0353
MyznEiji <em711th@icloud.com>
2020-09-01 00:20:01 +0000

    get data

--- Laravel_introduction/laraapp/backend/resources/views/person/index.blade.php
+++ Laravel_introduction/laraapp/backend/resources/views/person/index.blade.php
@@ -10,13 +10,16 @@
         <th>Name: </th>
         <th>Mail: </th>
         <th>Age: </th>
+        <th>get data method</th>
     </tr>
         @foreach ($items as $item)
         <tr>
             <td>{{$item->name}}</td>
             <td>{{$item->mail}}</td>
             <td>{{$item->age}}</td>
+            <td>{{$item->getData()}}</td>
         </tr>
+
         @endforeach
 
 
