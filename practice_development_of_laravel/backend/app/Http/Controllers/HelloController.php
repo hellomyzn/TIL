e5642b44800ab9ae320832f2cc92b34504aca978
MyznEiji <em711th@icloud.com>
2020-09-08 00:18:30 +0000

    service app

--- practice_development_of_laravel/backend/app/Http/Controllers/HelloController.php
+++ practice_development_of_laravel/backend/app/Http/Controllers/HelloController.php
@@ -6,185 +6,204 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 use App\Person;
+use App\MyClasses\MyService;
 
 
 class HelloController extends Controller
 {
 
-    # Section1
-    function __construct(){
-        config(['sample.message' => 'This message is from hello controller construct through config/sample.php']);
-    }
+    // # Section1
+    // function __construct(){
+    //     config(['sample.message' => 'This message is from hello controller construct through config/sample.php']);
+    // }
 
-    public function index(){
-        $data = [
-            'msg' => 'This is sample message from hello Controller',
-        ];
+    // public function index(){
+    //     $data = [
+    //         'msg' => 'This is sample message from hello Controller',
+    //     ];
 
-        return view('hello.index', compact('data'));
-    }
+    //     return view('hello.index', compact('data'));
+    // }
 
-    public function index_name(){
-        return view('hello.index_name');
-    }
+    // public function index_name(){
+    //     return view('hello.index_name');
+    // }
 
-    public function index_other(){
-        return redirect()->route('HI');
-    }
+    // public function index_other(){
+    //     return redirect()->route('HI');
+    // }
 
-    public function index_id($id){
-        return view('hello.index_id', compact('id'));
-    }
+    // public function index_id($id){
+    //     return view('hello.index_id', compact('id'));
+    // }
 
-    public function index_hello(Request $request){
-        $hello = $request->hello;
-        return view('hello.index_hello', compact('hello'));
-    }
+    // public function index_hello(Request $request){
+    //     $hello = $request->hello;
+    //     return view('hello.index_hello', compact('hello'));
+    // }
 
-    public function index_bye(Request $request){
-        $bye = $request->bye;
-        return view('hello.index_bye', compact('bye'));
-    }
+    // public function index_bye(Request $request){
+    //     $bye = $request->bye;
+    //     return view('hello.index_bye', compact('bye'));
+    // }
 
-    public function index_model(Person $person){
-        return view('hello.index_model',compact('person'));
-    }
+    // public function index_model(Person $person){
+    //     return view('hello.index_model',compact('person'));
+    // }
 
-    public function index_config(){
-        $sample_msg = config('sample.message');
-        $sample_data = config('sample.data');
-        return view('hello.index_config', compact('sample_msg', 'sample_data'));
-    }
+    // public function index_config(){
+    //     $sample_msg = config('sample.message');
+    //     $sample_data = config('sample.data');
+    //     return view('hello.index_config', compact('sample_msg', 'sample_data'));
+    // }
 
-    public function index_env(){
-        $sample_msg = env('SAMPLE_MESSAGE');
-        $sample_data = explode(',', env('SAMPLE_DATA'));
-        return view('hello.index_env', compact('sample_msg', 'sample_data'));
-    }
+    // public function index_env(){
+    //     $sample_msg = env('SAMPLE_MESSAGE');
+    //     $sample_data = explode(',', env('SAMPLE_DATA'));
+    //     return view('hello.index_env', compact('sample_msg', 'sample_data'));
+    // }
 
-    public function index_storage(){
-        $sample_msg = Storage::get('sample.txt');
-        return view('hello.index_storage', compact('sample_msg'));
-    }
+    // public function index_storage(){
+    //     $sample_msg = Storage::get('sample.txt');
+    //     return view('hello.index_storage', compact('sample_msg'));
+    // }
 
-    public function index_public(){
-        $sample_msg = Storage::disk('public')->url('sample.txt');
-        return view('hello.index_public', compact('sample_msg'));
-    }
+    // public function index_public(){
+    //     $sample_msg = Storage::disk('public')->url('sample.txt');
+    //     return view('hello.index_public', compact('sample_msg'));
+    // }
 
-    public function index_storage_delete(){
-        if (Storage::disk('public')->exists('bk_sample.txt'))
-        {
-            Storage::disk('public')->delete('bk_sample.txt');
-        }
-        Storage::disk('public')->copy('sample.txt','bk_sample.txt');
+    // public function index_storage_delete(){
+    //     if (Storage::disk('public')->exists('bk_sample.txt'))
+    //     {
+    //         Storage::disk('public')->delete('bk_sample.txt');
+    //     }
+    //     Storage::disk('public')->copy('sample.txt','bk_sample.txt');
 
-        if (Storage::disk('local')->exists('bk_sample.txt'))
-        {
-            Storage::disk('local')->delete('bk_sample.txt');
-        }
-        Storage::disk('local')->move('public/bk_sample.txt', 'bk_sample.txt');
-        return redirect('hello');
-    }
+    //     if (Storage::disk('local')->exists('bk_sample.txt'))
+    //     {
+    //         Storage::disk('local')->delete('bk_sample.txt');
+    //     }
+    //     Storage::disk('local')->move('public/bk_sample.txt', 'bk_sample.txt');
+    //     return redirect('hello');
+    // }
 
-    public function index_download_page(){
-        return view('hello.index_download');
-    }
+    // public function index_download_page(){
+    //     return view('hello.index_download');
+    // }
 
-    public function index_download(){
-        return Storage::disk('public')->download('sample.txt');
-    }
+    // public function index_download(){
+    //     return Storage::disk('public')->download('sample.txt');
+    // }
 
-    public function index_upload(){
-        return view('hello.index_upload');
-    }
-    public function index_save(Request $request){
-        Storage::disk('local')->putFile('files', $request->file('file'));
-        return redirect('hello/index_upload');
-    }
+    // public function index_upload(){
+    //     return view('hello.index_upload');
+    // }
+    // public function index_save(Request $request){
+    //     Storage::disk('local')->putFile('files', $request->file('file'));
+    //     return redirect('hello/index_upload');
+    // }
 
-    public function index_save_as(Request $request){
-        $ext = '.' . $request->file('file')->extension();
-        Storage::disk('public')->putFileAs('files', $request->file('file'), 'uploaded' . $ext);
-        return redirect('hello/index_upload');
-    }
+    // public function index_save_as(Request $request){
+    //     $ext = '.' . $request->file('file')->extension();
+    //     Storage::disk('public')->putFileAs('files', $request->file('file'), 'uploaded' . $ext);
+    //     return redirect('hello/index_upload');
+    // }
 
-    public function index_allfiles(){
-        $dir = '/';
-        $all = Storage::disk('logs')->allfiles($dir);
-
-
-        return view('hello/index_allfiles', compact('all'));
-    }
-
-    public function index_request(Request $request, Response $response){
-        $msg = 'please input something';
-        $keys = [];
-        $values = [];
-        if($request->isMethod('post'))
-        {
-            $msg = 'yout tyepd: '. $request->input('msg');
-            // $form = $request->all();
-            $form = $request->only(['msg', 'mail']);
-            $keys = array_keys($form);
-            $values = array_values($form);
-        }
+    // public function index_allfiles(){
+    //     $dir = '/';
+    //     $all = Storage::disk('logs')->allfiles($dir);
 
 
-        return view('hello/index_request', compact('msg', 'keys', 'values'));
-    }
+    //     return view('hello/index_allfiles', compact('all'));
+    // }
 
-    public function index_response(Request $request, Response $response){
-        $msg = 'please input something';
-        $keys = [];
-        $values = [];
-        if($request->isMethod('post'))
-        {
-            $msg = 'yout tyepd: '. $request->input('msg');
-            $form = $request->all();
-            $result ='<html><body>';
-            foreach($form as $key => $value)
-            {
-                $result .= $key . ': ' . $value . '<br>';
-            }
-            $result .= '</body></html>';
-            $response->setContent($result);
-            return $response;
-        }
+    // public function index_request(Request $request, Response $response){
+    //     $msg = 'please input something';
+    //     $keys = [];
+    //     $values = [];
+    //     if($request->isMethod('post'))
+    //     {
+    //         $msg = 'yout tyepd: '. $request->input('msg');
+    //         // $form = $request->all();
+    //         $form = $request->only(['msg', 'mail']);
+    //         $keys = array_keys($form);
+    //         $values = array_values($form);
+    //     }
 
 
-        return view('hello/index_request', compact('msg', 'keys', 'values'));
-    }
+    //     return view('hello/index_request', compact('msg', 'keys', 'values'));
+    // }
 
-    public function index_old(Request $request, Response $response){
-        $msg = 'please input something';
-        $keys = [];
-        $values = [];
-        if($request->isMethod('post'))
-        {
-            $msg = 'yout tyepd: '. $request->input('msg');
-            // $form = $request->all();
-            $form = $request->only(['msg', 'mail', 'tel']);
-            $keys = array_keys($form);
-            $values = array_values($form);
-            $msg = old('msg') . ', ' . old('mail') . ', ' . old('tel') . ', ';
-        }
+    // public function index_response(Request $request, Response $response){
+    //     $msg = 'please input something';
+    //     $keys = [];
+    //     $values = [];
+    //     if($request->isMethod('post'))
+    //     {
+    //         $msg = 'yout tyepd: '. $request->input('msg');
+    //         $form = $request->all();
+    //         $result ='<html><body>';
+    //         foreach($form as $key => $value)
+    //         {
+    //             $result .= $key . ': ' . $value . '<br>';
+    //         }
+    //         $result .= '</body></html>';
+    //         $response->setContent($result);
+    //         return $response;
+    //     }
 
-        $request->flash();
-        return view('hello/index_old', compact('msg', 'keys', 'values'));
-    }
 
-    public function index_query(Request $request, Response $response)
+    //     return view('hello/index_request', compact('msg', 'keys', 'values'));
+    // }
+
+    // public function index_old(Request $request, Response $response){
+    //     $msg = 'please input something';
+    //     $keys = [];
+    //     $values = [];
+    //     if($request->isMethod('post'))
+    //     {
+    //         $msg = 'yout tyepd: '. $request->input('msg');
+    //         // $form = $request->all();
+    //         $form = $request->only(['msg', 'mail', 'tel']);
+    //         $keys = array_keys($form);
+    //         $values = array_values($form);
+    //         $msg = old('msg') . ', ' . old('mail') . ', ' . old('tel') . ', ';
+    //     }
+
+    //     $request->flash();
+    //     return view('hello/index_old', compact('msg', 'keys', 'values'));
+    // }
+
+    // public function index_query(Request $request, Response $response)
+    // {
+    //     $name = $request->query('name');
+    //     $mail = $request->query('mail');
+    //     $tel = $request->query('tel');
+    //     $msg = $name . ', '. $mail . ', '. $tel . ', ';
+
+    //     $keys = ['Name', 'Mail', 'Tell'];
+    //     $values = [$name, $mail, $tel];
+    //     $request->flash();
+
+    //     return view('hello.index_query', compact('msg', 'keys', 'values'));
+    // }
+
+
+    # Section 2
+
+    public function index_service(MyService $myservice)
     {
-        $name = $request->query('name');
-        $mail = $request->query('mail');
-        $tel = $request->query('tel');
-        $msg = $name . ', '. $mail . ', '. $tel . ', ';
+        $msg = $myservice->say();
+        $data = $myservice->data();
+        return view('hello.index_service', compact('msg', 'data'));
+    }
 
-        $keys = ['Name', 'Mail', 'Tell'];
-        $values = [$name, $mail, $tel];
-        $request->flash();
-
-        return view('hello.index_query', compact('msg', 'keys', 'values'));
+    public function index_app()
+    {
+        $myservice = app('App\Myclasses\Myservice');
+        $msg = $myservice->say();
+        $data = $myservice->data();
+        return view('hello.index_service', compact('msg', 'data'));
     }
 }
--- practice_development_of_laravel/backend/routes/web.php
+++ practice_development_of_laravel/backend/routes/web.php
@@ -16,46 +16,51 @@ Route::get('/', function () {
 });
 
 
-# Section 1
+// # Section 1
 
-Route::get('hello/', 'HelloController@index');
+// Route::get('hello/', 'HelloController@index');
 
-Route::get('/hello/index_name', 'HelloController@index_name')->name('HI');
-Route::get('/hello/other', "HelloController@index_other");
+// Route::get('/hello/index_name', 'HelloController@index_name')->name('HI');
+// Route::get('/hello/other', "HelloController@index_other");
 
-Route::get('hello/index_id/{id}', 'HelloController@index_id')->where('id', '[0-9]+');
-Route::get('hello/index_hello', 'HelloController@index_hello')->middleware('hello');
-Route::get('hello/index_bye', 'HelloController@index_bye')->middleware('hello');
+// Route::get('hello/index_id/{id}', 'HelloController@index_id')->where('id', '[0-9]+');
+// Route::get('hello/index_hello', 'HelloController@index_hello')->middleware('hello');
+// Route::get('hello/index_bye', 'HelloController@index_bye')->middleware('hello');
 
-Route::namespace('Sample')->group(function() {
-    Route::get('/sample', "SampleController@index");
-    Route::get('sample/index_config', 'SampleController@index_config');
-});
+// Route::namespace('Sample')->group(function() {
+//     Route::get('/sample', "SampleController@index");
+//     Route::get('sample/index_config', 'SampleController@index_config');
+// });
 
-Route::get('hello/index_model/{person}', 'HelloController@index_model');
+// Route::get('hello/index_model/{person}', 'HelloController@index_model');
 
-Route::get('hello/index_config', 'HelloController@index_config');
-Route::get('hello/index_env', 'HelloController@index_env');
-Route::get('hello/index_storage', 'HelloController@index_storage');
-Route::get('hello/index_public', 'HelloController@index_public');
+// Route::get('hello/index_config', 'HelloController@index_config');
+// Route::get('hello/index_env', 'HelloController@index_env');
+// Route::get('hello/index_storage', 'HelloController@index_storage');
+// Route::get('hello/index_public', 'HelloController@index_public');
 
-Route::get('hello/index_storage_delete', 'HelloController@index_storage_delete');
-Route::get('hello/index_download_page', "HelloController@index_download_page");
-Route::get('hello/index_download', 'HelloController@index_download');
+// Route::get('hello/index_storage_delete', 'HelloController@index_storage_delete');
+// Route::get('hello/index_download_page', "HelloController@index_download_page");
+// Route::get('hello/index_download', 'HelloController@index_download');
 
-Route::get('hello/index_upload',"HelloController@index_upload");
-Route::post('hello/index_upload', 'HelloController@index_save');
-Route::post('hello/index_upload_as', 'HelloController@index_save_as');
+// Route::get('hello/index_upload',"HelloController@index_upload");
+// Route::post('hello/index_upload', 'HelloController@index_save');
+// Route::post('hello/index_upload_as', 'HelloController@index_save_as');
 
-Route::get('hello/index_allfiles',"HelloController@index_allfiles");
+// Route::get('hello/index_allfiles',"HelloController@index_allfiles");
 
-Route::get('hello/index_request', 'HelloController@index_request');
-Route::post('hello/index_request', 'HelloController@index_request');
+// Route::get('hello/index_request', 'HelloController@index_request');
+// Route::post('hello/index_request', 'HelloController@index_request');
 
-Route::get('hello/index_response', 'HelloController@index_response');
+// Route::get('hello/index_response', 'HelloController@index_response');
 
-Route::get('hello/index_old', 'HelloController@index_old');
-Route::post('hello/index_old', 'HelloController@index_old');
+// Route::get('hello/index_old', 'HelloController@index_old');
+// Route::post('hello/index_old', 'HelloController@index_old');
 
-Route::get('hello/index_query', 'HelloController@index_query');
-Route::post('hello/index_query', 'HelloController@index_query');
+// Route::get('hello/index_query', 'HelloController@index_query');
+// Route::post('hello/index_query', 'HelloController@index_query');
+
+
+# Section2
+Route::get('hello/index_service', "HelloController@index_service");
+Route::get('hello/index_app', "HelloController@index_app");
