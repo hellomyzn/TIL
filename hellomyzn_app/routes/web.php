5b3a91748951b5554312e477b44d0e44dd481ad7
MyznEiji <em711th@icloud.com>
2021-02-04 09:41:32 +0000

    fix: routes

--- hellomyzn_app/routes/web.php
+++ hellomyzn_app/routes/web.php
@@ -12,15 +12,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.index');
+})->name('index');
 
-Auth::routes();
+Route::get('posts/create', function(){
+    return view('pages.post.create');
+})->name('posts.create');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
+Route::get('users/1', function(){
+    return view('pages.user.show');
+})->name('users.show');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
