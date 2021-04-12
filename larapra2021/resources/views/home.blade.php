42d66d52db5a915930b28c8502556d68d290cf21
hellomyzn <em711th@icloud.com>
2021-04-13 08:09:32 +0900

    log out

--- larapra2021/resources/views/home.blade.php
+++ larapra2021/resources/views/home.blade.php
@@ -8,5 +8,20 @@
 </head>
 <body>
     Hello!
+
+    @auth
+        {{ Auth::user()->name }}
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
+                {{ __('Log Out') }}
+            </a>
+        </form>
+    @else
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+            @endif
+    @endauth
 </body>
 </html>
