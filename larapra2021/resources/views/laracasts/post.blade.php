193f21cb6d39d51d7bb8e39274c908fda61679df
hellomyzn <em711th@icloud.com>
2021-05-06 06:55:39 +0900

    post

---
+++ larapra2021/resources/views/laracasts/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('/css/laracasts/posts.css' ) }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1> <a href="/post"> My first post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quidem consectetur tenetur, illum hic accusamus numquam sequi officiis enim laudantium a? Cum debitis tempore in non quas adipisci quam magni?</p>
+    </article>
+</body>
+</html>
