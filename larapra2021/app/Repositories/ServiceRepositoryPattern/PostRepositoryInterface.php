1d44f660f89619981c31358e471750e4f6274b87
hellomyzn <em711th@icloud.com>
2021-08-12 20:29:53 +0900

    service repository pattern: comment

--- larapra2021/app/Repositories/ServiceRepositoryPattern/PostRepositoryInterface.php
+++ larapra2021/app/Repositories/ServiceRepositoryPattern/PostRepositoryInterface.php
@@ -5,13 +5,43 @@ namespace App\Repositories\ServiceRepositoryPattern;
 
 interface PostRepositoryInterface
 {
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function getAll();
 
+    /**
+     * Undocumented function
+     *
+     * @param int $id
+     * @return void
+     */
     public function getById($id);
 
+    /**
+     * Undocumented function
+     *
+     * @param array $data
+     * @return void
+     */
     public function save($data);
 
+    /**
+     * Undocumented function
+     *
+     * @param array $data
+     * @param int $id
+     * @return void
+     */
     public function update($data, $id);
 
+    /**
+     * Undocumented function
+     *
+     * @param int $id
+     * @return void
+     */
     public function delete($id);
 }
