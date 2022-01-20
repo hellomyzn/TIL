9490781c12bf7aacc7739cf1ba90107b511ff0a6
hellomyzn <em711th@icloud.com>
2022-01-20 10:05:48 +0900

    logging

--- simplenote/backend/database/seeders/DatabaseSeeder.php
+++ simplenote/backend/database/seeders/DatabaseSeeder.php
@@ -3,9 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 use App\Models\User;
 use App\Models\Memo;
 use App\Models\Tag;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +20,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create();
-        Memo::factory(10)->create();
+        User::factory(1)->create([
+            'name' => 'hoge',
+            'email' => 'hoge@hoge.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
         Tag::factory(10)->create();
+        Memo::factory(10)->create();
     }
 }
--- simplenote/backend/routes/web.php
+++ simplenote/backend/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,4 +21,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
