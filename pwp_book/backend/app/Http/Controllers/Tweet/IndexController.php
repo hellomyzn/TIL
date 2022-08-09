1c7e93670dc6edec51d2301f61114fad500a5af5
hellomyzn <em711th@icloud.com>
2022-08-09 13:52:01 +0900

    add: types of return to view

--- pwp_book/backend/app/Http/Controllers/Tweet/IndexController.php
+++ pwp_book/backend/app/Http/Controllers/Tweet/IndexController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\View\Factory;
 
 class IndexController extends Controller
-{
+{    
     /**
-     * Handle the incoming request.
+     * __invoke
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  mixed $request
+     * @param  mixed $factory
+     * @return void
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Factory $factory)
     {
-        RETURN VIEW('tweet.index', ['name' => 'laravel']);
+        // return View::make('tweet.index', ['name' => 'laravel']);
+        // return $factory->make('tweet.index', ['name' => 'laravel']);
+        // return view('tweet.index')->with('name', 'laravel');
+        return view('tweet.index')->with('name', 'laravel')->with('version', '8');
     }
 }
