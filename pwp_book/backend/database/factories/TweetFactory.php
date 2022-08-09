854a0c4a9f23d32f56efd833fcefeae5474b7145
hellomyzn <em711th@icloud.com>
2022-08-09 14:24:53 +0900

    tweet factory

---
+++ pwp_book/backend/database/factories/TweetFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * TweetFactory
+ */
+class TweetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'content' => $this->faker->realText(100)
+        ];
+    }
+}
