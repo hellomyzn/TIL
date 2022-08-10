da77029b2c3ae663a6a52eb0228990db262027a5
hellomyzn <em711th@icloud.com>
2022-08-10 16:46:49 +0900

    add layout compnent

--- pwp_book/backend/resources/views/tweet/index.blade.php
+++ pwp_book/backend/resources/views/tweet/index.blade.php
@@ -1,25 +1,16 @@
-<x-layout title="TOP | Tweet App">
-    hoge
+<x-layout title="TOP | つぶやきアプリ">
+    <x-layout.single>
+        <h2 class="text-center text-blue-500 text-4xl font-bold mt-8 mb-8">
+            つぶやきアプリ
+        </h2>
+        <x-tweet.form.post></x-tweet.form.post>
+    </x-layout.single>
 </x-layout>
-<h1>Tweets</h1>
-@auth
-    <div>
-        <p>Tweet Form</p>
-        @if (session('feedback.success'))
-            <p style='color: green'> {{ session('feedback.success') }} </p>
-        @endif
-        <form action="{{ route('tweet.create') }}" method="post">
-            @csrf
-            <label for="tweet-content">Tweet</label>
-            <span>*Until 140 characters</span>
-            <textarea name="tweet" id="tweet-content" type='text' placeholder='Write down here.'></textarea>
-            @error('tweet')
-                <p style="color: red;"> {{ $message }} </p>
-            @enderror
-            <button type='submit'>Send</button>
-        </form>
-    </div>    
-@endauth
+<x-tweet.form.post>
+    @if (session('feedback.success'))
+        <p style='color: green'> {{ session('feedback.success') }} </p>
+    @endif
+</x-tweet.form.post>
 
 @foreach ($tweets as $tweet)
     <details>
