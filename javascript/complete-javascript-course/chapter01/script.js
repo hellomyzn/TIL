674d89c444a495f03a31d4e913bed815acb381c2
hellomyzn <em711th@icloud.com>
2023-03-15 11:25:05 +0900

    finish: ep: 10

---
+++ javascript/complete-javascript-course/chapter01/script.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+// if (js === 'amazing') alert('JavaScript is Fun!');
+
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+let $age = 27;
+
+let PI = "3.1415";
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(firstName);
+// let 3years = 2;
+
