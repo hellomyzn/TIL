98bdfe74810e0925a55080999f3dd7d4f65458ac
hellomyzn <em711th@icloud.com>
2023-03-15 11:40:23 +0900

    finish: ep:12

--- javascript/complete-javascript-course/chapter01/script.js
+++ javascript/complete-javascript-course/chapter01/script.js
@@ -5,6 +5,7 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
 
+// EP: 10
 let firstName = "Jonas";
 let $age = 27;
 
@@ -16,3 +17,26 @@ let myCurrentJob = "Teacher";
 console.log(firstName);
 // let 3years = 2;
 
+// EP: 12
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+
+javascriptIsFun = 'Yes';
+console.log(typeof javascriptIsFun);
+
+// Undifined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
