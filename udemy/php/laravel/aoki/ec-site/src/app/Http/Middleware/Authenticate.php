11d1c6202ab1df43c12522f8bf4c4585f6fade52
hellomyzn <em711th@icloud.com>
2023-03-16 18:53:06 +0900

    update(laravel/ec): middleware and request for admin and owners

--- udemy/php/laravel/aoki/ec-site/src/app/Http/Middleware/Authenticate.php
+++ udemy/php/laravel/aoki/ec-site/src/app/Http/Middleware/Authenticate.php
@@ -4,14 +4,24 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class Authenticate extends Middleware
 {
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    protected $admin_route = 'admin.login';
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (Route::is('owner.*')){
+            return $request->expectsJson() ? null : route($this->owner_route);
+        } elseif(Route::is('admin.*')) {
+            return $request->expectsJson() ? null : route($this->admin_route);
+        } else {
+            return $request->expectsJson() ? null : route($this->user_route);
+        }
     }
 }
