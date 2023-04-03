b58108d6cc32b72efc990a27cce79ec9c6fed18b
hellomyzn <em711th@icloud.com>
2023-04-03 14:08:25 +0900

    update(python-intro1): section3

---
+++ pdf/python/python-intro1/src/section3/main.py
@@ -0,0 +1,20 @@
+# 算術基礎
+# pra3_1
+a_san = 65
+b_san = 78
+c_san = 92
+sum = a_san + b_san + c_san
+ave = sum / 3
+print(ave)
+
+# pra3_2
+price = 980
+price *= 1.08
+price //= 1
+print(price)
+
+# pra3_3
+height = 173
+weight = 80
+bmi = weight / ((height / 100) ** 2)
+print(f'BMI = {bmi}')
