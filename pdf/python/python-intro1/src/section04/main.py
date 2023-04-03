0d38f8d065e533a0b8c573dfe855c3e67e2d7008
hellomyzn <em711th@icloud.com>
2023-04-03 14:19:15 +0900

    update(python-intro1): section4

---
+++ pdf/python/python-intro1/src/section04/main.py
@@ -0,0 +1,30 @@
+# リストo
+# pra4_1
+any = [3, 11, 7, 5, 13]
+print(any[0])
+print(any[1])
+print(any[2])
+print(any[3])
+print(any[4])
+# print(any[5])
+
+# pra4_2
+print(f'anyの要素数は {len(any)} 個')
+
+# pra4_3
+print(any)
+del any[0]
+print(any)
+
+# pra4_4
+address = ['Shinjyuku-ku', 'Tokyo-to', 'Japan']
+print(address[0], ':', len(address[0]))
+print(address[1], ':', len(address[1]))
+print(address[2], ':', len(address[2]))
+
+# q4
+total = 0
+total = sum(any)
+print(f'合計は{total}です')
+
+
