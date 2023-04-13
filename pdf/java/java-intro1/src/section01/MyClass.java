9233306bcee4932a7a8f3884476468d7b7333c04
hellomyzn <hellomyzn@gmail.com>
2023-04-13 14:06:59 +0900

    update(java-intro1): sec1

--- pdf/java/java-intro1/src/section01/MyClass.java
+++ pdf/java/java-intro1/src/section01/MyClass.java
@@ -20,5 +20,15 @@ public class MyClass {
         sx = 0x10;
         sy = 0x20;
         System.out.println("sx="+sx+" sy="+sy);
+
+        // e1
+        double pai;
+        pai = 3.141592;
+        System.out.println("pai =" +pai);
+
+        int jupyter_distance;
+        jupyter_distance = 750000000;
+        System.out.println("jupyter_distance =" +jupyter_distance);
+
     }
 }
