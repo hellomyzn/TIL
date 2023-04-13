cb9ba711eaade674e71dd37e8a0d1cbdfff1bace
hellomyzn <hellomyzn@gmail.com>
2023-04-13 13:53:36 +0900

    update java

---
+++ pdf/java/java-intro1/src/section01/MyClass.java
@@ -0,0 +1,24 @@
+// 変数
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        // p1_1
+        int ix, iy;
+        ix = 10;
+        iy = 20;
+        System.out.println("ix="+ix+" iy="+iy);
+
+        // p1_2
+        double dx, dy;
+        dx = 10.5;
+        dy = 20.25;
+        System.out.println("dx="+dx+" dy="+dy);
+
+        // p1_2
+        short sx, sy;
+        sx = 0x10;
+        sy = 0x20;
+        System.out.println("sx="+sx+" sy="+sy);
+    }
+}
