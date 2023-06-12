2d03772dc00e26aa973ea521ce6f7880f26e0325
hellomyzn <hellomyzn@gmail.com>
2023-06-13 08:10:33 +0900

    add readme and notes

--- books/algorithm/tessoku/backend/chapter01/05_three_cards.py
+++ books/algorithm/tessoku/backend/chapter01/05_three_cards.py
@@ -1,11 +1,10 @@
 N, K = map(int, input().split())
-
 ans = 0
+
 for r in range(1, N+1):
     for b in range(1, N+1):
-        w = K - r - b
-        if 1 <= w <= N:
+        sum = K - r - b
+        if 1 <= sum and sum <= N:
             ans += 1
 
 print(ans)
-
