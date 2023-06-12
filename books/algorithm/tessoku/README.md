61a734c13616f0fd3ff1c6fd6ca47c28e4d5e933
hellomyzn <em711th@icloud.com>
2023-06-13 08:10:33 +0900

    add readme and notes

---
+++ books/algorithm/tessoku/README.md
@@ -0,0 +1,4 @@
+[github](https://github.com/E869120/kyopro-tessoku)
+[atcorder](https://atcoder.jp/contests/tessoku-book)
+[youtube](https://www.youtube.com/playlist?list=PL3Hpv03CoZ26J0wwIBTQ3Ge5gT9SMCLdQ)
+
--- books/algorithm/tessoku/backend/chapter02/06_how_many_guests.py
+++ books/algorithm/tessoku/backend/chapter02/06_how_many_guests.py
@@ -1,17 +1,6 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
-
-accum = [0] * N
-
-accum[0] = A[0]
-for i in range(1, N):
-    accum[i] = accum[i-1] + A[i]
-
-for _ in range(Q):
-    L, R = map(int, input().split())
-    L -= 1
-    R -= 1
-    ans = 0
-    for i in range(L, R+1):
-        ans += A[i]
-    print(ans)
+L = [None] * Q
+R = [None] * Q
+for i in range(Q):
+    L[i], R[i] = map(int, input().split())
