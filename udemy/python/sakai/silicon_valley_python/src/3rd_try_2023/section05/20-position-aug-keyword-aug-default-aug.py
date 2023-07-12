0d5f2a3861a934b63138e738697d5eab8048c243
hellomyzn <em711th@icloud.com>
2023-07-13 08:06:32 +0900

    add (python sakai): finish sec5 20

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section05/20-position-aug-keyword-aug-default-aug.py
@@ -0,0 +1,8 @@
+def menu(entree='beef', drink='wine', desert='pudding'):
+    print(entree)
+    print(drink)
+    print(desert)
+
+menu(entree='beef', desert='ice', drink='beer')
+menu('beef', desert='ice', drink='beer')
+menu()
