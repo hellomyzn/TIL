c10180e8373c663c8d7342699b184d763c77d962
hellomyzn <em711th@icloud.com>
2023-07-13 08:20:47 +0900

    add (python sakai): finish sec5 27

--- udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section05/27-decorator.py
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section05/27-decorator.py
@@ -1,14 +1,33 @@
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print('func:', func.__name__)
+        print('args:', args)
+        print('kwargs:', kwargs)
+        result = func(*args, **kwargs)
+        print('result:', result)
+        return result
+    return wrapper
+
+
 def print_info(func):
     def wrapper(*args, **kwargs):
         print('start')
-        r = func(*args, **kwargs)
+        result = func(*args, **kwargs)
         print('end')
-        return r
+        return result
     return wrapper
 
+
+@print_info
+@print_more
 def add_num(a, b):
     return a + b
 
-f = print_info(add_num)
-r = f(10, 20)
-print(r)
+
+R = add_num(10, 20)
+print(R)
+
+
+# f = print_info(add_num)
+# r = f(10, 20)
+# print(r)
