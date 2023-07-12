1e7e11d7db76b538854bcf1296e11854239c6549
hellomyzn <em711th@icloud.com>
2023-07-13 08:07:09 +0900

    add (python sakai): finish sec5 27

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section05/27-decorator.py
@@ -0,0 +1,14 @@
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        r = func(*args, **kwargs)
+        print('end')
+        return r
+    return wrapper
+
+def add_num(a, b):
+    return a + b
+
+f = print_info(add_num)
+r = f(10, 20)
+print(r)
