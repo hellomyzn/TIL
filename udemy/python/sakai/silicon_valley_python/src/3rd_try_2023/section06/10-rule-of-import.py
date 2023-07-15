5f0290d11ca780debf9e59dc9a575e3915f4e61d
hellomyzn <em711th@icloud.com>
2023-07-15 10:02:53 +0900

    add (python sakai): finish sec6 10

---
+++ udemy/python/sakai/silicon_valley_python/src/3rd_try_2023/section06/10-rule-of-import.py
@@ -0,0 +1,18 @@
+# built-in library
+import collections
+import os
+import sys
+
+# 3rd party library
+import termcolor
+
+# my own library
+from tools import utils
+
+print(collections.__file__)
+print(os.__file__)
+print(termcolor.__file__)
+print(utils.__file__)
+
+
+print(sys.path)
