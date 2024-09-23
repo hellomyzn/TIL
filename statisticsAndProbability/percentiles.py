6edff88c5bf28533a5383d31528b53983a49eb79
hellomyzn <em711th@icloud.com>
2024-09-23 16:27:32 +0900

    delete: u-practical-data-science

--- statisticsAndProbability/percentiles.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-vals = np.random.normal(0, 0.5, 10000)
-plt.hist(vals, 50, edgecolor="k")
-np.percentile(vals, 50)
-np.percentile(vals, 90)
-np.percentile(vals, 20)
