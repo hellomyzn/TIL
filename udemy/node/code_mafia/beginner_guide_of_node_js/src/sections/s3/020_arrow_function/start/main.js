1cb6414d23747872221d2fe5800d86c9dadc48d7
hellomyzn <em711th@icloud.com>
2024-10-18 19:04:20 +0900

    update: udemy node s3 arrow func

--- udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s3/020_arrow_function/start/main.js
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s3/020_arrow_function/start/main.js
@@ -3,8 +3,9 @@ function fn(number) {
 }
 
 console.log(fn(2));
-const fnArrow = (number) => {
-  return number * 2;
-};
+
+const fnArrow = (number) => number * 2;
+const fnArrowObj = (number) => ({ result: number * 2 });
 
 console.log(fnArrow(2));
+console.log(fnArrowObj(2));
