4f2be874bcdc3dbb0b510e6c808e0d6d0637d41c
hellomyzn <em711th@icloud.com>
2024-10-19 15:58:03 +0900

    update: udemy node

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s3/050_destructuring/main.js
@@ -0,0 +1,27 @@
+// const [a, b, c] = ["配列1", "配列2", "配列3"];
+// console.log(a);
+// console.log(c);
+
+// const { x, z } = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
+// console.log(x);
+// console.log(z);
+
+const arr = ["Japan", "Tokyo", "Shinjuku"];
+const objAddress = { country: "Japan", state: "Tokyo", city: "Shinjuku" };
+
+const fnArr = (arry) => {
+  console.log("---配列---");
+  console.log(`country: ${arry[0]}`);
+  console.log(`state: ${arry[1]}`);
+  console.log(`city: ${arry[2]}`);
+};
+
+const fnObj = (objAddr) => {
+  console.log("---オブジェクト---");
+  console.log(`country: ${objAddr.country}`);
+  console.log(`state: ${objAddr.state}`);
+  console.log(`city: ${objAddr.city}`);
+};
+
+fnArr(arr);
+fnObj(objAddress);
