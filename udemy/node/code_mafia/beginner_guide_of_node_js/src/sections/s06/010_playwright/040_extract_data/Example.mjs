e1dfc637af9a0eadf0d75fd790346bb7b7ab51eb
hellomyzn <em711th@icloud.com>
2024-10-22 17:06:50 +0900

    update: udemy node playwright locator

--- udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s06/010_playwright/040_extract_data/Example.mjs
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s06/010_playwright/040_extract_data/Example.mjs
@@ -1,12 +1,25 @@
 import { chromium } from "@playwright/test";
 
 (async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 500 });
+  const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
+
+  // css
+  const pageTitleLocator = await page.locator(".navbar-brand");
+  const pageTitle = await pageTitleLocator.innerText();
+  console.log(pageTitle);
+
+  // 文字列で要素を取得
+  const textLocator = await page.locator("text=名刺管理アプリ");
+  const pageText = await textLocator.innerText();
+  console.log(pageText);
+
+  // xpathで要素を取得 (xpathはclass,idがない時に使用する)
+  const xpathLocator = await page.locator('xpath=//*[@id="__next"]/nav/div/a');
+  const pageXpath = await xpathLocator.innerText();
+  console.log(pageXpath);
+
   const htmlStr = await page.content();
-
-  console.log(htmlStr);
-
   await browser.close();
 })();
