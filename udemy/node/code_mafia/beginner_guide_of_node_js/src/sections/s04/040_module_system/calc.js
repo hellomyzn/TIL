43b122989750cc0e902279d9e2598fda986a3305
hellomyzn <em711th@icloud.com>
2024-10-22 09:28:37 +0900

    update: udemy node module

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s04/040_module_system/calc.js
@@ -0,0 +1,9 @@
+const plus = (a, b) => a + b;
+function minus(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  plus: plus,
+  minus,
+};
---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s04/040_module_system/example.js
@@ -0,0 +1,6 @@
+const calc = require("./calc");
+const { minus } = require("./calc");
+const result = calc.plus(1, 2);
+const result2 = minus(1, 2);
+console.log(result);
+console.log(result2);
