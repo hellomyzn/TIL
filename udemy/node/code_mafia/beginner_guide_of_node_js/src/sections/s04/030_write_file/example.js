f23c40e95747906d29656042e2db0f5567369f52
hellomyzn <em711th@icloud.com>
2024-10-22 09:10:07 +0900

    update: udemy node path

--- udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s04/030_write_file/example.js
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s04/030_write_file/example.js
@@ -3,4 +3,8 @@ console.log(__dirname);
 console.log(__filename);
 
 const fs = require("fs");
+const path = require("path");
+const distPath = path.resolve(__dirname, "./hoge/hogehoge.txt");
+console.log(distPath);
 fs.writeFileSync(`${__dirname}/hoge.txt`, "hello, node.js");
+fs.writeFileSync(distPath, "hello, node.js");
