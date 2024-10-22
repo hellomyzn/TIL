4170de182638835bbb7c57de536f5f314000e95b
hellomyzn <em711th@icloud.com>
2024-10-22 15:26:29 +0900

    update: udemy node dependencies devDependencies

--- udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s05/010_intro/main.mjs
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s05/010_intro/main.mjs
@@ -1,10 +1,12 @@
 // import _ from "./lib/lodash.js";
-import _ from "lodash";
+// import _ from "lodash";
+import cloneDeep from "lodash/cloneDeep.js";
 
 const original = { prop: { nested: "value" } };
 // オブジェクトの複製
 
-const cloned = _.cloneDeep(original);
+// const cloned = _.cloneDeep(original);
+const cloned = cloneDeep(original);
 cloned.prop.nested = "new value";
 console.log(original);
 console.log(cloned);
