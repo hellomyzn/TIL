bd4afb0574e6d1a5fb4cce4c5b9181c9a629ad1f
hellomyzn <em711th@icloud.com>
2024-10-23 15:43:32 +0900

    update: udemy node ui events

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s06/010_playwright/047_ui_events/Example.mjs
@@ -0,0 +1,24 @@
+import { chromium } from "@playwright/test";
+
+// @see セレクターのチェーンの利用方法(>>)
+// https://playwright.dev/docs/selectors#chaining-selectors
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+
+  const inputLocator = page.locator('//*[@id="__next"]/div/div[1]/label/input');
+  await inputLocator.type("美");
+
+  // const pager3Locator = page.locator(".page-link.page-number >> nth=2");
+  const pager3Locator = page.locator(".page-link.page-number >> nth=-1");
+  await pager3Locator.click();
+
+  const cardLocator = page.locator(".cards.list-group-item");
+
+  const cardCount = await cardLocator.count();
+  console.log(cardCount);
+
+  await browser.close();
+})();
