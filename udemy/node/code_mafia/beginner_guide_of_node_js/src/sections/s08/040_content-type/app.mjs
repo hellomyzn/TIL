6a5fc78d582bcdaec836dffecbe96e3cc526482d
hellomyzn <em711th@icloud.com>
2024-10-24 08:41:23 +0900

    update: node back content-type

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s08/040_content-type/app.mjs
@@ -0,0 +1,25 @@
+import * as http from "http";
+
+const server = http.createServer(function (req, res) {
+  console.log(req.url);
+  //   res.writeHead(200, { "content-type": "text/html; charset=UTF-8" });
+  if (req.url === "/hello") {
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Hello</h1>
+        </body>
+        </html>
+        `);
+  } else if (req.url === "/bye") {
+    res.end("bye");
+  }
+});
+
+server.listen(3000);
