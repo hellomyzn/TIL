cb1141212cda4f93194e0f023fda61cc976ccd7b
hellomyzn <em711th@icloud.com>
2024-10-24 08:21:46 +0900

    update: udemy node back browser

--- udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s08/030_browser_and_server/app.mjs
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s08/030_browser_and_server/app.mjs
@@ -6,10 +6,10 @@ import * as http from "http";
 const server = http.createServer(function (req, res) {
   console.log(req.url);
   if (req.url === "/hello") {
-    res.end("hello");
+    res.end('<script>window.alert("frontend")</script>');
   } else if (req.url === "/bye") {
     res.end("bye");
   }
 });
 
-server.listen(8080);
+server.listen(3000);
