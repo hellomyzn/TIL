986c485dfc42705c49eedaa268e56f5862c427a7
hellomyzn <em711th@icloud.com>
2024-10-24 07:22:54 +0900

    update: udemy node gss practice

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s06/020_google_sheet/030_practice/Example.mjs
@@ -0,0 +1,53 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import env from "dotenv";
+env.config();
+import { createRequire } from "module";
+import { workerData } from "worker_threads";
+const require = createRequire(import.meta.url);
+const secrets = require("../google_secrets.json");
+
+/**
+ * 問題：
+ * 以下の値をシートを完成させてください。
+ * * `cart` シートの作成、name, priceの挿入は手動で行ってください。
+ * name   | price
+ * Orange | 120
+ * Banana | 50
+ * Apple  | 100
+ * 合計    | 270
+ */
+(async () => {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+
+  await doc.useServiceAccountAuth({
+    client_email: secrets.client_email,
+    private_key: secrets.private_key,
+  });
+
+  await doc.loadInfo();
+
+  await doc.addSheet({
+    title: "cart",
+    headerValues: ["name", "price"],
+  });
+  const cartSheet = doc.sheetsByTitle["cart"];
+
+  const rows = await cartSheet.addRows([
+    {
+      name: "Orange",
+      price: 120,
+    },
+    {
+      name: "Orange",
+      price: 50,
+    },
+    {
+      name: "Orange",
+      price: 100,
+    },
+    {
+      name: "合計",
+      price: "=sum(B2:B4)",
+    },
+  ]);
+})();
