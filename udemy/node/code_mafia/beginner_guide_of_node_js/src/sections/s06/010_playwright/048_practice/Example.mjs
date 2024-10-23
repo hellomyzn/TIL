6bfc04335bfa2f7c622fbca62e9bc12afb3561c6
hellomyzn <em711th@icloud.com>
2024-10-23 15:58:35 +0900

    update: udemy node playwright practice

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s06/010_playwright/048_practice/Example.mjs
@@ -0,0 +1,27 @@
+import { chromium } from "@playwright/test";
+
+/**
+ * 練習問題
+ * [佐藤]で検索して、一番最後に出てくる人物の名前を取得してください。
+ */
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+
+  const inputLocator = page.locator('//*[@id="__next"]/div/div[1]/label/input');
+  await inputLocator.type("佐藤");
+
+  const pagerLocator = page.locator(".page-link.page-number");
+  if ((await pagerLocator.count()) > 1) {
+    const lastPagerLocator = page.locator("nth=-1");
+    await lastPagerLocator.click();
+  }
+
+  const lastCardlocator = page.locator(".cards.list-group-item >> nth=-1");
+  const lastCardName = await lastCardlocator.textContent();
+
+  console.log(lastCardName);
+
+  await browser.close();
+})();
