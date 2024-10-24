6cf31b0046e4ed7cb39f4a9f054fb6c961d7dad5
hellomyzn <em711th@icloud.com>
2024-10-24 14:36:13 +0900

    update: udemy node express practice

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s08/080_express_practice/app.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.send(`
+    <h1>練習問題</h1>
+    <p>下記のフォームで送信したリクエストを「商品1、商品2がカートに追加されました。」のようなレスポンスとして返却する処理を記述しましょう</p>
+    <form action="/cart" method="POST">
+    <div>
+      <label>商品：<input type="text" name="product[]"></label>
+    </div>
+    <div>
+      <label>商品：<input type="text" name="product[]"></label>
+    </div>
+    <input type="submit">
+    </form>
+    `);
+});
+
+app.post("/cart", (req, res) => {
+  const product = req.body.product;
+  res.send(`${product.join(",")}がカートに追加されました。`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server start: http://localhost:${PORT}`);
+});
