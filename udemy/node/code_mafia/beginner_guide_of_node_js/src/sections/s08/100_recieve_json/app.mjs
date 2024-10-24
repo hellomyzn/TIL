584136b03dff610c0632a01fa7878abeda8379c3
hellomyzn <em711th@icloud.com>
2024-10-24 14:36:42 +0900

    update: udemy node back express recieve json

---
+++ udemy/node/code_mafia/beginner_guide_of_node_js/src/sections/s08/100_recieve_json/app.mjs
@@ -0,0 +1,50 @@
+import * as http from "http";
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send(`
+    <form action="/result" method="POST">
+      <input type="text" name="title">
+      <input type="text" name="description">
+      <input type="submit">
+    </form>
+    <script>
+      const formEl = document.querySelector("form")
+      formEl.onsubmit = function(event){
+        event.preventDefault();
+        const title =  formEl[0].value
+        const desc =  formEl[1].value
+
+        const data = {
+          title,
+          desc
+        }
+
+        fetch('/result', {
+          method: "POST"
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify(data)
+        }).then(() => {
+          return res.json();
+        })
+      }
+    </script>
+    `);
+});
+
+app.post("/result", function (req, res) {
+  const params = req.body;
+  console.log(params);
+  res.json({ msg: "success" });
+});
+
+app.listen(PORT, function () {
+  console.log(`Server start: http://localhost:${PORT}`);
+});
