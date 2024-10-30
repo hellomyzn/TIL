ab423b99d3d839cbf8949eeea09e87f39df995e2
hellomyzn <em711th@icloud.com>
2024-10-30 15:14:26 +0900

    update: menta js basic2

--- menta/fumiya_ito/javascript/src/basic2_JavaScriptのオブジェクト・基礎 2/basic2.js
+++ menta/fumiya_ito/javascript/src/basic2_JavaScriptのオブジェクト・基礎 2/basic2.js
@@ -129,3 +129,89 @@ var minutes = date.getMinutes(); // 分を取得（0から59）
 var seconds = date.getSeconds(); // 秒を取得（0から59）
 console.log(`${hours}:${minutes}:${seconds}`); // 例：12:34:56
 // 指定されている時刻の情報を取得するためのインスタンスメソッドgetHours()、getMinutes()、getSeconds()を使用して、時、分、秒の情報を取得しています。
+
+// Mathオブジェクト
+// MathオブジェクトはJSに用意されている数学的な処理をするためのもの。
+
+// Math.abs(a):  引数で渡した数値の絶対値を返す。
+// Math.round(a): 引数で渡した数値を四捨五入する。
+// Math.ceil(a): 引数で渡した数値の小数点を切り上げる。
+// Math.floor(a): 引数で渡した数値の小数点を切り捨てる。
+// max(a, b, c): 引数で渡した数値の最大の数値を返す。
+// min(a, b, c): 引数で渡した数値の最小の数値を返す。
+// random(): 0以上1未満の乱数を生成する。
+
+Math.abs(-2); // 2
+Math.round(1.666); // 2
+Math.ceil(1.666); // 2
+Math.floor(1.666); // 1
+Math.max(1, 2, 3, 4); // 4
+Math.min(1, 2, 3, 4); // 1
+Math.random(); // 0~1のランダムな数
+
+// Math.max()
+// Math.max()を使用して最大値を取得することができます。
+
+// 例1: number配列の中の最大値を取得
+var number = [3, 1, 13, 32, 27];
+var maxNumber = Math.max(...number);
+
+console.log(maxNumber); // 32
+// スプレッド演算子を使用して、number配列の要素を可変長引数としてMath.maxメソッドに渡しています。
+
+// Math.min()
+// 最小値を取得するには、Math.min()メソッドを使用します。
+
+// 例1: 最小値を取得
+var num1 = 10;
+var num2 = 5;
+var num3 = 20;
+
+var minNum = Math.min(num1, num2, num3);
+
+console.log(minNum); // 5
+// num1、num2、num3のうち最小値をMath.min()メソッドで取得し、minNumに代入しています
+
+// Math.floor()
+// Mathオブジェクトを使用して、数値を整数にするには、Math.floorメソッドを使用します。
+
+// 例1: Math.floor()を使用して、数値を整数にする
+var num1 = 3.14;
+var num2 = 5.87;
+
+var intNum1 = Math.floor(num1);
+var intNum2 = Math.floor(num2);
+
+console.log(intNum1); // 3
+console.log(intNum2); // 5
+// Math.floorメソッドは、引数として与えられた数値を小数点以下を切り捨てて最大の整数に丸めた値を返します。
+
+// Math.ceil()
+// Mathオブジェクトを使用して、数値を整数にするには、Math.ceilメソッドも使用できます。
+
+// 例1: Math.ceil()を使用して、数値を整数にする
+var num3 = 3.14;
+var num4 = 5.87;
+
+var intNum3 = Math.ceil(num3);
+var intNum4 = Math.ceil(num4);
+
+console.log(intNum3); // 4
+console.log(intNum4); // 6
+// Math.ceilメソッドは、引数として与えられた数値を小数点以下を切り上げて最小の整数に丸めた値を返します。
+
+// Math.random()
+// Math.random()を使用して、乱数を生成することができます。Math.random()は0〜1未満（1は入らない）までの小数による乱数を生成します。
+
+//生成した乱数を変数randomに代入する
+var random = Math.random();
+
+console.log(random);
+// 範囲を決めた乱数を作るときには、Math.random()に最大値を掛けることで実装できます。
+
+// 例1: 0 〜 10の範囲で乱数を作成
+var random = Math.floor(Math.random() * 11);
+
+console.log(random);
+// Math.random()は0〜1未満（0〜0.9999････）の小数を返すので、最大値の10をそのまま掛けてしまうと0 〜 9の範囲になってしまう。そのため、最大値に1を足して「11」を掛けることで、0 〜 10の範囲を設定しています。
+// また、整数にするには、小数点を切り捨てれば良いので、同じくMathオブジェクトで提供されているMath.floorメソッドを使用します。
