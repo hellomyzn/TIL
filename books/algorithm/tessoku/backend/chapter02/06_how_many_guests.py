09187fdc25ef9a8eca17f8d8558a276d2fdb79bb
hellomyzn <em711th@icloud.com>
2024-11-23 11:15:22 +0900

    update: book tessoku

--- books/algorithm/tessoku/backend/chapter02/06_how_many_guests.py
+++ books/algorithm/tessoku/backend/chapter02/06_how_many_guests.py
@@ -1,6 +1,15 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
-L = [None] * Q
-R = [None] * Q
-for i in range(Q):
-    L[i], R[i] = map(int, input().split())
+
+accum = [0] * N
+
+accum[0] = A[0]
+for i in range(1, N):
+    accum[i] = accum[i-1] + A[i]
+
+for _ in range(Q):
+    L, R = map(int, input().split())
+    L -= 1
+    R -= 1
+    ans = accum[R] - (accum[L-1] if L > 0 else 0)
+    print(ans)
