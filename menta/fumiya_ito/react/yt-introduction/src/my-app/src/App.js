db4b8a579d03a31fc3ce363fc263a7fc3bfc0a57
hellomyzn <em711th@icloud.com>
2024-12-16 14:51:40 +0900

    update: menta react state

--- menta/fumiya_ito/react/yt-introduction/src/my-app/src/App.js
+++ menta/fumiya_ito/react/yt-introduction/src/my-app/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Button from "./Button";
+import Counter from "./components/Counter";
 
 const App = () => {
   return (
     <div>
-      <div>App</div>
       <Button title="post" />
       <Button title="edit" />
       <Button title="delete" />
       <Button title="submit" />
+      <Counter />
     </div>
   );
 };
