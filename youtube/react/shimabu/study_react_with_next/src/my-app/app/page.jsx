3fc72636ab13f6aa81b6240d01d901d8f28cfb74
hellomyzn <em711th@icloud.com>
2024-12-21 17:03:58 +0900

    update: yt next onClick, useCallback

--- youtube/react/shimabu/study_react_with_next/src/my-app/app/page.jsx
+++ youtube/react/shimabu/study_react_with_next/src/my-app/app/page.jsx
@@ -1,11 +1,20 @@
+"use client";
+import { useCallback } from "react";
+
 import { Main } from "@components/Main";
 import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
 
 export default function Home() {
+  const handleClick = useCallback(() => {
+    console.log("hoge");
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
+      <a href="/" onClick={handleClick}>
+        button
+      </a>
       <Main title="Index Page" page="app/page.js" />
       <Footer />
     </div>
