a64b56cf1666206a5ae44a0f6ab9f4e96cb5676e
hellomyzn <em711th@icloud.com>
2024-12-21 17:34:28 +0900

    update: yt next counter use state

--- youtube/react/shimabu/study_react_with_next/src/my-app/app/about/page.jsx
+++ youtube/react/shimabu/study_react_with_next/src/my-app/app/about/page.jsx
@@ -1,8 +1,18 @@
+"use client";
+import { useCallback, useEffect } from "react";
+
 import { Main } from "@components/Main";
 import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
 
 export default function About() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
--- youtube/react/shimabu/study_react_with_next/src/my-app/app/page.jsx
+++ youtube/react/shimabu/study_react_with_next/src/my-app/app/page.jsx
@@ -1,28 +1,22 @@
 "use client";
-import { useCallback, useEffect } from "react";
+import { useState } from "react";
 
 import { Main } from "@components/Main";
 import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
 
 export default function Home() {
-  const handleClick = useCallback(() => {
-    console.log("hoge");
-  }, []);
+  const [counter, setCounter] = useState(1);
+  const handleClick = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    console.log(counter);
+  };
 
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <a href="/" onClick={handleClick}>
-        button
-      </a>
+      <h1>{counter}</h1>
+      <button onClick={() => handleClick()}>button</button>
       <Main title="Index Page" page="app/page.js" />
       <Footer />
     </div>
