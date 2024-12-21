5adba4b5b1b68a93e7a6616d71fb05ea64c48404
hellomyzn <em711th@icloud.com>
2024-12-21 16:05:46 +0900

    update: yt next link components

--- youtube/react/shimabu/study_react_with_next/src/my-app/app/about/page.js
+++ youtube/react/shimabu/study_react_with_next/src/my-app/app/about/page.js
@@ -1,9 +1,11 @@
 import { Main } from "@components/Main";
 import { Footer } from "@components/Footer";
+import { Header } from "@components/Header";
 
 export default function About() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Header />
       <Main title="About Page" page="app/about/page.js" />
       <Footer />
     </div>
