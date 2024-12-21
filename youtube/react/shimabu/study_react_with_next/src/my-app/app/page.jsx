24a0f5238138462c35aa4855b263eb876fc536fc
hellomyzn <em711th@icloud.com>
2024-12-21 18:07:50 +0900

    update: yt next useState with string and bool

--- youtube/react/shimabu/study_react_with_next/src/my-app/app/page.jsx
+++ youtube/react/shimabu/study_react_with_next/src/my-app/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import { Main } from "@components/Main";
 import { Footer } from "@components/Footer";
@@ -7,16 +7,34 @@ import { Header } from "@components/Header";
 
 export default function Home() {
   const [counter, setCounter] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(false);
+
   const handleClick = () => {
     setCounter((prevCounter) => prevCounter + 1);
     console.log(counter);
   };
 
+  const handleChange = useCallback((e) => {
+    if (e.target.value >= 5) {
+      alert("hoge");
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleToggleButton = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <h1>{counter}</h1>
+      {isShow ? <h1>{counter}</h1> : null}
       <button onClick={() => handleClick()}>button</button>
+      <button onClick={handleToggleButton}>
+        {isShow ? "hide counter" : "show counter"}
+      </button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main title="Index Page" page="app/page.js" />
       <Footer />
     </div>
