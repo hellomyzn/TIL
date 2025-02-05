3e274581e12f71dcf4838707e88d68502fe91a37
hellomyzn <em711th@icloud.com>
2025-02-05 17:09:38 +0900

    update: udemy next microblog: ch34

--- udemy/react/shin_code/nextjs13_learning_with_microblog/src/blogAPI.ts
+++ udemy/react/shin_code/nextjs13_learning_with_microblog/src/blogAPI.ts
@@ -27,3 +27,28 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json();
   return article;
 };
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDatetime = new Date().toISOString();
+
+  const res = await fetch(`http://localhost:3001/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, title, content, currentDatetime }),
+  });
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) throw new Error("An error has occurred.");
+
+  const article = await res.json();
+  return article;
+};
