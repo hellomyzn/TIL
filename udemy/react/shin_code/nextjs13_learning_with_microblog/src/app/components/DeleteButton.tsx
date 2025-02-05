91690010daf85599e4010a270266b70790378b4d
hellomyzn <em711th@icloud.com>
2025-02-05 17:44:45 +0900

    update: udemy next microblog: ch38

--- udemy/react/shin_code/nextjs13_learning_with_microblog/src/app/components/DeleteButton.tsx
+++ udemy/react/shin_code/nextjs13_learning_with_microblog/src/app/components/DeleteButton.tsx
@@ -1,8 +1,23 @@
 "use client";
+import { deleteArticle } from "@/blogAPI";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const DeleteButton = ({ id }: { id: string }) => {
-  return <div>DeleteButton</div>;
+  const router = useRouter();
+  const handleDelete = async () => {
+    await deleteArticle(id);
+    router.push("/");
+    router.refresh();
+  };
+  return (
+    <div
+      onClick={handleDelete}
+      className="bg-red-500 hover:bg-red-600 rounded-md py-2 px-5 inline cursor-pointer"
+    >
+      Delete
+    </div>
+  );
 };
 
 export default DeleteButton;
