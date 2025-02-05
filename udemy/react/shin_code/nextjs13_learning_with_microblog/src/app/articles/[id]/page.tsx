ba1e384d149d35b59b1f92931db6903d0860c4e9
hellomyzn <em711th@icloud.com>
2025-02-05 16:19:38 +0900

    update: udemy next microblog: ch29

--- udemy/react/shin_code/nextjs13_learning_with_microblog/src/app/articles/[id]/page.tsx
+++ udemy/react/shin_code/nextjs13_learning_with_microblog/src/app/articles/[id]/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import type { Article } from "@/types";
+import { getDetailArticle } from "@/blogAPI";
 
-const Article = ({ params }: { params: Article }) => {
+const Article = async ({ params }: { params: { id: string } }) => {
+  const detailArticle = await getDetailArticle(params.id);
+  console.log(detailArticle);
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
--- udemy/react/shin_code/nextjs13_learning_with_microblog/src/blogAPI.ts
+++ udemy/react/shin_code/nextjs13_learning_with_microblog/src/blogAPI.ts
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import type { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, {
@@ -10,4 +11,19 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article[]> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 }, //ISR
+  });
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) throw new Error("An error has occurred.");
+
+  const article = await res.json();
+  return article;
 };
