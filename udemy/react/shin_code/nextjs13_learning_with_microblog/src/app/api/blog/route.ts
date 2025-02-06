01325323559216874ff52d0e297e2e134227c9c8
hellomyzn <em711th@icloud.com>
2025-02-06 17:06:34 +0900

    update: udemy next microblog: ch49

--- udemy/react/shin_code/nextjs13_learning_with_microblog/src/app/api/blog/route.ts
+++ udemy/react/shin_code/nextjs13_learning_with_microblog/src/app/api/blog/route.ts
@@ -9,7 +9,25 @@ export async function GET(req: Request, res: NextApiResponse) {
   }
 
   return NextResponse.json(
-    { message: "success", articles: data },
+    { message: "success to get all articles", articles: data },
     { status: 200 }
+  );
+}
+
+export async function POST(req: Request, res: NextApiResponse) {
+  const { id, title, content } = await req.json();
+  const createdAt = new Date().toISOString();
+
+  const { data, error } = await supabase
+    .from("posts")
+    .insert([{ id, title, content, createdAt }]);
+
+  if (error) {
+    return NextResponse.json(error);
+  }
+
+  return NextResponse.json(
+    { message: "success to post a article", articles: data },
+    { status: 201 }
   );
 }
