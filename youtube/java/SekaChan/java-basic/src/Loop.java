120a93e8303180e31bf11d962377c5b8562ebc72
hellomyzn <em711th@icloud.com>
2025-03-27 16:40:50 +0900

    update: yt java basic loop

--- youtube/java/SekaChan/java-basic/src/Loop.java
+++ youtube/java/SekaChan/java-basic/src/Loop.java
@@ -1,8 +1,7 @@
 public class Loop {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 3) {
-            prin
+        for (int i = 0; i < 3; i++) {
+            System.out.println("i = " + i);
         }
         
     }
