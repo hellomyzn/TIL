ad942d72e29222518f8a33b82acea9a42b77f21f
hellomyzn <em711th@icloud.com>
2025-03-31 10:58:52 +0900

    start: yt java spring env

---
+++ youtube/java/juniordevmind/simple-spring-rest-api/simple-spring-rest-api/src/main/java/com/example/simple_spring_rest_api/controllers/TodoController.java
@@ -0,0 +1,14 @@
+
+@RestController
+public class TodoController {
+    // get todos
+    public String getTodoItems(){
+        return "get todo items"
+    }
+
+
+    // get todo
+    // create todo
+    // update todo
+    // delete todo
+}
