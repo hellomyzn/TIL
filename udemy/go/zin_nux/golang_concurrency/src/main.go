7525ebd5bdaaaa5ade6b1a35a52770ac592446bc
hellomyzn <em711th@icloud.com>
2025-10-06 11:25:32 +0900

    update: udemy go; vid 7

--- udemy/go/zin_nux/golang_concurrency/src/main.go
+++ udemy/go/zin_nux/golang_concurrency/src/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-basics/calculator"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load()
+	fmt.Println(os.Getenv("GO_ENV"))
+
+	var a float64 = 1
+	var b float64 = 2
+
+	fmt.Println("offset: ", calculator.Offset)
+	fmt.Println("a:", a, "\nb ", b)
+	fmt.Println(calculator.Sum(a, b))
+	fmt.Println(calculator.Multiply(a, b))
 
 }
