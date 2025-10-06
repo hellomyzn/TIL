068d7df73652cfd34821a39995417b060f144e77
hellomyzn <em711th@icloud.com>
2025-10-06 11:46:38 +0900

    update: udemy go: vid 8

--- udemy/go/zin_nux/golang_concurrency/src/main.go
+++ udemy/go/zin_nux/golang_concurrency/src/main.go
@@ -1,25 +1,71 @@
 package main
 
-import (
-	"fmt"
-	"go-basics/calculator"
-	"os"
+// const secret = "abc"
 
-	"github.com/joho/godotenv"
+// type Os int
+
+// const (
+// 	Mac Os = iota + 1
+// 	Windows
+// 	Linux
+// )
+
+var (
+	i int
+	s string
+	b bool
 )
 
 func main() {
-	fmt.Println("hello world")
+	// fmt.Println("hello world")
 
-	godotenv.Load()
-	fmt.Println(os.Getenv("GO_ENV"))
+	// godotenv.Load()
+	// fmt.Println(os.Getenv("GO_ENV"))
 
-	var a float64 = 1
-	var b float64 = 2
+	// var a float64 = 1
+	// var b float64 = 2
 
-	fmt.Println("offset: ", calculator.Offset)
-	fmt.Println("a:", a, "\nb ", b)
-	fmt.Println(calculator.Sum(a, b))
-	fmt.Println(calculator.Multiply(a, b))
+	// fmt.Println("offset: ", calculator.Offset)
+	// fmt.Println("a:", a, "\nb ", b)
+	// fmt.Println(calculator.Sum(a, b))
+	// fmt.Println(calculator.Multiply(a, b))
+
+	// var i int
+	// var i2 int = 2
+	// var i3 = 3
+	// i4 := 4
+	// ui := uint16(5)
+	// fmt.Println(i)
+	// fmt.Println(i2)
+	// fmt.Println(i3)
+	// fmt.Println(i4)
+	// fmt.Println(ui)
+
+	// fmt.Printf("i: %v %T\n", i, i)
+	// fmt.Printf("i: %[1]v %[1]T ui: %[2]v %[2]T\n", i, ui)
+
+	// f := 1.2345
+	// s := "hello"
+	// b := true
+	// fmt.Printf("f: %[1]v %[1]T \n", f)
+	// fmt.Printf("s: %[1]v %[1]T \n", s)
+	// fmt.Printf("b: %[1]v %[1]T \n", b)
+
+	// pi, title := 3.14, "Go"
+	// fmt.Printf("pi: %v title: %v\n", pi, title)
+
+	// x := 10
+	// y := 1.23
+	// z := float64(x) + y
+	// println(z)
+
+	// fmt.Printf("Mac: %v Win: %v Lin: %v \n", Mac, Windows, Linux)
+
+	// var var1 int = 1
+	// fmt.Println((var1))
+	// var1 = 2
+	// fmt.Println((var1))
+	// var1 += 1
+	// fmt.Println((var1))
 
 }
