de27fd15c81df30d3b6c8658730bd39cb8df29fc
hellomyzn <em711th@icloud.com>
2025-10-13 15:32:29 +0900

    update: udemy go: vid 9

--- udemy/go/zin_nux/golang_concurrency/src/main.go
+++ udemy/go/zin_nux/golang_concurrency/src/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"unsafe"
+)
+
 // const secret = "abc"
 
 // type Os int
@@ -10,11 +15,11 @@ package main
 // 	Linux
 // )
 
-var (
-	i int
-	s string
-	b bool
-)
+// var (
+// 	i int
+// 	s string
+// 	b bool
+// )
 
 func main() {
 	// fmt.Println("hello world")
@@ -67,5 +72,40 @@ func main() {
 	// fmt.Println((var1))
 	// var1 += 1
 	// fmt.Println((var1))
+
+	var ui1 uint16
+	fmt.Printf("memory address of ui1: %p\n", &ui1)
+
+	var ui2 uint16
+	fmt.Printf("memory address of ui2: %p\n", &ui2)
+
+	var p1 *uint16
+	fmt.Printf("value of p1: %v\n", p1)
+	p1 = &ui1
+	fmt.Printf("value of p1: %v\n", p1)
+	fmt.Printf("size of p1: %d[bytes]\n ", unsafe.Sizeof(p1))
+	fmt.Printf("memory address of p1: %p\n", &p1)
+	fmt.Printf("value of ui1(dereference): %v\n", *p1)
+
+	var pp1 **uint16 = &p1
+	fmt.Printf("value of pp1: %v\n", pp1)
+	fmt.Printf("size of pp1: %d[bytes]\n ", unsafe.Sizeof(pp1))
+	fmt.Printf("memory address of pp1: %p\n", &pp1)
+
+	fmt.Printf("value of p1(dereference): %v\n", *pp1)
+	fmt.Printf("value of ui1(dereference): %v\n", **pp1)
+
+	ok, result := true, "A"
+	fmt.Printf("memory address of result: %p\n", &result)
+	if ok {
+		result := "B"
+		fmt.Printf("memory address of result: %p\n", &result)
+		println(result)
+	} else {
+		result := "C"
+		println(result)
+	}
+
+	println(result)
 
 }
